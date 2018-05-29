872c34f097c0144dd5f2da8ef9fb0c9c461bcfe4
Dave Cheney <dave@cheney.net>
2018-05-29 15:51:41 +1000

    README: remove references to non RBAC configurations
    Fixes #341
    
    Heptio's policy is that all k8s clusters should be running RBAC. In
    support of that policy remove all the non RBAC deployment examples and
    documentation.
    
    Signed-off-by: Dave Cheney <dave@cheney.net>

--- deployment/render.sh
+++ deployment/render.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
 ls ds-grpc-v2/*.yaml | xargs cat common/gen-warning.yaml > render/daemonset-rbac.yaml
-ls ds-grpc-v2/*.yaml | grep -v rbac | xargs cat common/gen-warning.yaml > render/daemonset-norbac.yaml
 
 ls deployment-grpc-v2/*.yaml | xargs cat common/gen-warning.yaml > render/deployment-rbac.yaml
-ls deployment-grpc-v2/*.yaml | grep -v rbac | xargs cat common/gen-warning.yaml > render/deployment-norbac.yaml
