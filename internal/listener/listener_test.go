edc66097c14c5a89515275ef39d68fcb7bdd09c6
Dave Cheney <dave@cheney.net>
2018-06-26 18:15:23 +1000

    internal/listener: add test for ingressroute https
    Signed-off-by: Dave Cheney <dave@cheney.net>

--- internal/listener/listener_test.go
+++ internal/listener/listener_test.go
@@ -181,6 +181,63 @@ func TestListenerVisit(t *testing.T) {
 				},
 			},
 		},
+		"simple ingressroute with secret": {
+			objs: []interface{}{
+				&ingressroutev1.IngressRoute{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "simple",
+						Namespace: "default",
+					},
+					Spec: ingressroutev1.IngressRouteSpec{
+						VirtualHost: &ingressroutev1.VirtualHost{
+							Fqdn: "www.example.com",
+							TLS: &ingressroutev1.TLS{
+								SecretName: "secret",
+							},
+						},
+						Routes: []ingressroutev1.Route{
+							{
+								Services: []ingressroutev1.Service{
+									{
+										Name: "backend",
+										Port: 80,
+									},
+								},
+							},
+						},
+					},
+				},
+				&v1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "secret",
+						Namespace: "default",
+					},
+					Data: secretdata("certificate", "key"),
+				},
+			},
+			want: map[string]*v2.Listener{
+				ENVOY_HTTP_LISTENER: &v2.Listener{
+					Name:    ENVOY_HTTP_LISTENER,
+					Address: socketaddress("0.0.0.0", 8080),
+					FilterChains: []listener.FilterChain{
+						filterchain(false, httpfilter(ENVOY_HTTP_LISTENER, DEFAULT_HTTP_ACCESS_LOG)),
+					},
+				},
+				ENVOY_HTTPS_LISTENER: &v2.Listener{
+					Name:    ENVOY_HTTPS_LISTENER,
+					Address: socketaddress("0.0.0.0", 8443),
+					FilterChains: []listener.FilterChain{{
+						FilterChainMatch: &listener.FilterChainMatch{
+							SniDomains: []string{"www.example.com"},
+						},
+						TlsContext: tlscontext(secretdata("certificate", "key"), auth.TlsParameters_TLSv1_1, "h2", "http/1.1"),
+						Filters: []listener.Filter{
+							httpfilter(ENVOY_HTTPS_LISTENER, DEFAULT_HTTPS_ACCESS_LOG),
+						},
+					}},
+				},
+			},
+		},
 	}
 
 	for name, tc := range tests {
