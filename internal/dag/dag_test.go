efaf6dd1ef6ea693d6a7566a8d8853989ea90a34
Dave Cheney <dave@cheney.net>
2019-08-22 14:23:22 +1000

    internal/dag: move validity check to VirtualHost/SecureVirtualHost
    Updates #1165
    
    The rules for determining if a SecureVirtualHost is valid are subtle as
    they must take into account the workarounds for TLS passthrough to a
    proxy. Move those into methods on VirtualHost/SecureVirtualHost and add
    tests to assert them.
    
    Signed-off-by: Dave Cheney <dave@cheney.net>

---
+++ internal/dag/dag_test.go
@@ -0,0 +1,101 @@
+// Copyright © 2019 Heptio
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dag
+
+import (
+	"testing"
+)
+
+func TestVirtualHostValid(t *testing.T) {
+	assert := Assert{t}
+
+	vh := VirtualHost{}
+	assert.False(vh.Valid())
+
+	vh = VirtualHost{
+		routes: map[string]Vertex{
+			"/": &Route{},
+		},
+	}
+	assert.True(vh.Valid())
+
+	vh = VirtualHost{
+		TCPProxy: new(TCPProxy),
+	}
+	assert.True(vh.Valid())
+}
+
+func TestSecureVirtualHostValid(t *testing.T) {
+	assert := Assert{t}
+
+	vh := SecureVirtualHost{}
+	assert.False(vh.Valid())
+
+	vh = SecureVirtualHost{
+		Secret: new(Secret),
+	}
+	assert.True(vh.Valid())
+
+	vh = SecureVirtualHost{
+		VirtualHost: VirtualHost{
+			routes: map[string]Vertex{
+				"/": &Route{},
+			},
+		},
+	}
+	assert.False(vh.Valid())
+
+	vh = SecureVirtualHost{
+		Secret: new(Secret),
+		VirtualHost: VirtualHost{
+			routes: map[string]Vertex{
+				"/": &Route{},
+			},
+		},
+	}
+	assert.True(vh.Valid())
+
+	vh = SecureVirtualHost{
+		VirtualHost: VirtualHost{
+			TCPProxy: new(TCPProxy),
+		},
+	}
+	assert.True(vh.Valid())
+
+	vh = SecureVirtualHost{
+		Secret: new(Secret),
+		VirtualHost: VirtualHost{
+			TCPProxy: new(TCPProxy),
+		},
+	}
+	assert.True(vh.Valid())
+}
+
+type Assert struct {
+	*testing.T
+}
+
+func (a Assert) True(t bool) {
+	a.Helper()
+	if !t {
+		a.Error("expected true, got false")
+	}
+}
+
+func (a Assert) False(t bool) {
+	a.Helper()
+	if t {
+		a.Error("expected false, got true")
+	}
+}
