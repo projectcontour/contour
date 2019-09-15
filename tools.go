abbb481da653034486772c672c4f1654645a48d5
Dave Cheney <dave@cheney.net>
2019-09-16 08:30:41 +1000

    tools.go: gofmt
    Signed-off-by: Dave Cheney <dave@cheney.net>

--- tools.go
+++ tools.go
@@ -3,16 +3,16 @@
 package tools
 
 import (
-	_ "mvdan.cc/unparam"
-	_ "honnef.co/go/tools/cmd/staticcheck"
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/gordonklaus/ineffassign"
-	_ "github.com/mdempsky/unconvert"
 	_ "github.com/kisielk/errcheck"
+	_ "github.com/mdempsky/unconvert"
+	_ "honnef.co/go/tools/cmd/staticcheck"
+	_ "mvdan.cc/unparam"
 
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/defaulter-gen"
-	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "k8s.io/code-generator/cmd/informer-gen"
+	_ "k8s.io/code-generator/cmd/lister-gen"
 )
