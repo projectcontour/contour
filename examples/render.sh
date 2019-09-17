c5a9faf4e67667df87a44ff2a8676b112936e82e
Dave Cheney <dave@cheney.net>
2019-09-17 10:39:09 +1000

    examples: remove deprecated examples
    Fixes #1418
    Updates #1505
    
    Rather than updating the namespace for our deprecated examples, just
    delete them.
    
    Remove deployment-grpc-v2, ds-grpc-v2, ds-hostnet, and ds-hostnet-split.
    Move common files into contour, remove common.
    Update render.sh, remove deprecated rendered files.
    
    The renders referenced in the quickstart README point to the
    release-0.15 branch and are unaffected by this change.
    
    Signed-off-by: Dave Cheney <dave@cheney.net>

--- examples/render.sh
+++ examples/render.sh
@@ -1,7 +1,3 @@
 #! /bin/sh
 
-ls ds-grpc-v2/*.yaml | xargs cat common/gen-warning.yaml > render/daemonset-rbac.yaml
-
-ls deployment-grpc-v2/*.yaml | xargs cat common/gen-warning.yaml > render/deployment-rbac.yaml
-
-ls contour/*.yaml | xargs cat common/gen-warning.yaml > render/contour.yaml
+ls contour/*.yaml | xargs cat render/gen-warning.yaml > render/contour.yaml
