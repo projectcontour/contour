6bc51405b30aeb127d133caa750c683a46161715
Nick Young <ynick@vmware.com>
2019-10-29 11:37:53 +1100

    Update render.sh to change ImagePullPolicy to IfNotPresent
    For examples/render/contour.yaml only.
    
    Fixes #1528.
    
    Signed-off-by: Nick Young <ynick@vmware.com>

--- examples/render.sh
+++ examples/render.sh
@@ -1,3 +1,6 @@
 #! /usr/bin/env bash
 
-ls contour/*.yaml | xargs cat render/gen-warning.yaml > render/contour.yaml
+ls contour/*.yaml | \
+  xargs cat render/gen-warning.yaml | \
+  sed 's/imagePullPolicy: Always/imagePullPolicy: IfNotPresent/g' \
+  > render/contour.yaml
