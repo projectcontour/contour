6903d27a03f8569bb6dc1bfe7353cad357960013
Brett Johnson <brett@sdbrett.com>
2019-11-04 14:21:03 +1100

    Added travis job to proof check site configuration
    Changes enable new travis job which performs changes against the
    site to help detect broken links.
    
    Additional script provides functionality to determine if a travis
    job should run by files changes in commit.
    
    This closes #1848.
    
    Signed-off-by: Brett Johnson <brett@sdbrett.com>

---
+++ hack/travis/travis-check-files-changed.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+
+# Checks if the files changed in the last commit are contained within
+# specifed directories.  This is used to determine if a job should
+# run based on changes within a commit PATHS_TO_SEARCH can be a single
+# path "site" or multiple paths "site hack", response if for any path
+# specified.
+
+# Add to a job within .travis.yml
+#      env:
+#        - SEARCH_DIRECTORIES="site"
+#      install:
+#        - ./hack/travis/travis-check-files-changed.sh $SEARCH_DIRECTORIES ; RETURN_CODE=$? ; if [ $RETURN_CODE -eq 137 ]; then travis_terminate 0; elif [ $RETURN_CODE -ne 0 ]; then travis_terminate $RETURN_CODE; fi
+
+set -e # halt script on error
+
+# 1. Make sure the paths to search are not empty
+if [ $# -eq 0 ]; then
+    echo "usage: $0 DIRECTORY [DIRECTORY...]"
+    exit 1
+fi
+
+# 2. Get the latest commit
+LATEST_COMMIT=$(git rev-parse HEAD)
+
+# 3. Get the latest commit in the searched paths
+LATEST_COMMIT_IN_PATH=$(git log -1 --format=format:%H --full-diff "$@")
+
+if [ $LATEST_COMMIT != $LATEST_COMMIT_IN_PATH ]; then
+    echo "Exiting this job because code in the following paths have not changed:"
+    echo $@
+    exit 137
+fi
+
+echo "Changes detected in the following paths:"
+echo $@
+exit 0
