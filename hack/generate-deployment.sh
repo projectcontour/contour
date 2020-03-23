f6f3ced2ceb7d3877d134d77651ff31495eafc74
James Peach <jpeach@vmware.com>
2020-03-24 08:15:04 +1100

    site: remove misleading comment in example deployment
    Remove the misleading comment in the example deployment that guided
    readers to a script that no longer exists. Update the generator script
    to be completely self-contained.
    
    Signed-off-by: James Peach <jpeach@vmware.com>

--- hack/generate-deployment.sh
+++ hack/generate-deployment.sh
@@ -6,8 +6,26 @@ set -o pipefail
 
 readonly HERE=$(cd $(dirname $0) && pwd)
 readonly REPO=$(cd ${HERE}/.. && pwd)
+readonly PROGNAME=$(basename $0)
 
-ls ${REPO}/examples/contour/*.yaml | \
-  xargs cat ${REPO}/examples/render/gen-warning.yaml | \
-  sed 's/imagePullPolicy: Always/imagePullPolicy: IfNotPresent/g' \
-  > ${REPO}/examples/render/contour.yaml
+
+readonly TARGET="${REPO}/examples/render/contour.yaml"
+
+exec >$TARGET
+
+cat <<EOF
+# This file is generated from the individual YAML files by $PROGNAME. Do not
+# edit this file directly but instead edit the source files and re-render.
+#
+# Generated from:
+EOF
+
+(cd ${REPO} && ls examples/contour/*.yaml) | \
+  awk '{printf "#       %s\n", $0}'
+
+echo "#"
+echo
+
+cat ${REPO}/examples/contour/*.yaml | \
+  sed 's/imagePullPolicy: Always/imagePullPolicy: IfNotPresent/g'
+
