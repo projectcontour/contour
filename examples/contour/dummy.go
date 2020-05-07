5bfca997de6f7c29b0cea214674f3fe41a7f3425
Matt Moore <mattmoor@vmware.com>
2020-05-06 18:13:08 -0700

    Add build tag
    Signed-off-by: Matt Moore <mattmoor@vmware.com>

--- examples/contour/dummy.go
+++ examples/contour/dummy.go
@@ -1,3 +1,5 @@
+// +build vendormagic
+
 // Copyright © 2020 VMware
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
