0d6f0384470fc4e2b0d94410a5edda2dbc0ba603
Nick Young <ynick@vmware.com>
2020-05-27 06:53:42 +1000

    Adds support for updating HTTPProxy addresses. (#2551)
    This is step 2 in a three phase plan.
    
    Adds the `status.loadBalancer` struct to HTTPProxy.
    
    Also, updates the StatusAddressUpdater with HTTPProxy support.
    This will probably be the last type added there for a while.
    
    I experimented with adding a `kubectl get` column for the addresses, but the
    multi-value nature of the loadBalancer.ingress slice is not accessible via a simple
    jsonPath expression, so we can't get all the values without special logic (which I
    assume Ingress has).
    
    Address details are available via `kubectl describe` from the command line.
    
    Updates #2371.
    
    Signed-off-by: Nick Young <ynick@vmware.com>
    
    Co-authored-by: Steve Sloka <slokas@vmware.com>

--- internal/k8s/statusaddress.go
+++ internal/k8s/statusaddress.go
@@ -14,10 +14,14 @@
 package k8s
 
 import (
+	"fmt"
+
+	projcontour "github.com/projectcontour/contour/apis/projectcontour/v1"
 	"github.com/projectcontour/contour/internal/annotation"
 	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/api/networking/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 // StatusAddressUpdater observes informer OnAdd and OnUpdate events and
@@ -30,69 +34,91 @@ type StatusAddressUpdater struct {
 	LBStatus      v1.LoadBalancerStatus
 	IngressClass  string
 	StatusUpdater StatusUpdater
+	Converter     Converter
 }
 
 func (s *StatusAddressUpdater) OnAdd(obj interface{}) {
 
-	ing := obj.(*v1beta1.Ingress).DeepCopy()
-	ing.SetGroupVersionKind(v1beta1.SchemeGroupVersion.WithKind("ingress"))
-	if !annotation.MatchesIngressClass(ing, s.IngressClass) {
+	obj, err := s.Converter.FromUnstructured(obj)
+	if err != nil {
+		s.Logger.Error("unable to convert object from Unstructured")
+		return
+	}
+
+	var typed Object
+	var gvr schema.GroupVersionResource
+	var kind string
+
+	switch o := obj.(type) {
+	case *v1beta1.Ingress:
+		o.GetObjectKind().SetGroupVersionKind(v1beta1.SchemeGroupVersion.WithKind("ingress"))
+		typed = o.DeepCopy()
+		gvr = v1beta1.SchemeGroupVersion.WithResource("ingresses")
+		kind = "ingress"
+	case *projcontour.HTTPProxy:
+		o.GetObjectKind().SetGroupVersionKind(projcontour.SchemeGroupVersion.WithKind("httpproxy"))
+		typed = o.DeepCopy()
+		gvr = projcontour.SchemeGroupVersion.WithResource("httpproxies")
+		kind = "httpproxy"
+	default:
 		s.Logger.
-			WithField("name", ing.GetName()).
-			WithField("namespace", ing.GetNamespace()).
-			WithField("ingress-class", annotation.IngressClass(ing)).
-			WithField("configured-ingress-class", s.IngressClass).
+			Debug("unsupported type received")
+		return
+	}
+
+	if !annotation.MatchesIngressClass(typed, s.IngressClass) {
+		s.Logger.
+			WithField("name", typed.GetObjectMeta().GetName()).
+			WithField("namespace", typed.GetObjectMeta().GetNamespace()).
+			WithField("ingress-class", annotation.IngressClass(typed)).
+			WithField("defined-ingress-class", s.IngressClass).
+			WithField("kind", kind).
 			Debug("unmatched ingress class, skip status update")
 		return
 	}
 
+	s.Logger.
+		WithField("name", typed.GetObjectMeta().GetName()).
+		WithField("namespace", typed.GetObjectMeta().GetNamespace()).
+		WithField("ingress-class", annotation.IngressClass(typed)).
+		WithField("kind", kind).
+		WithField("defined-ingress-class", s.IngressClass).
+		Debug("Received an object, sending status update")
+
 	s.StatusUpdater.Update(
-		ing.GetName(),
-		ing.GetNamespace(),
-		v1beta1.SchemeGroupVersion.WithResource("ingresses"),
-		StatusMutatorFunc(ingressMutator(s.LBStatus)),
+		typed.GetObjectMeta().GetName(),
+		typed.GetObjectMeta().GetNamespace(),
+		gvr,
+		StatusMutatorFunc(func(obj interface{}) interface{} {
+			switch o := obj.(type) {
+			case *v1beta1.Ingress:
+				dco := o.DeepCopy()
+				dco.Status.LoadBalancer = s.LBStatus
+				return dco
+			case *projcontour.HTTPProxy:
+				dco := o.DeepCopy()
+				dco.Status.LoadBalancer = s.LBStatus
+				return dco
+			default:
+				panic(fmt.Sprintf("Unsupported object %s/%s in status Address mutator",
+					typed.GetObjectMeta().GetName(), typed.GetObjectMeta().GetNamespace(),
+				))
+			}
+		}),
 	)
 }
 
 func (s *StatusAddressUpdater) OnUpdate(oldObj, newObj interface{}) {
 
-	newIng := newObj.(*v1beta1.Ingress).DeepCopy()
-	newIng.SetGroupVersionKind(v1beta1.SchemeGroupVersion.WithKind("ingress"))
-	if annotation.MatchesIngressClass(newIng, s.IngressClass) {
-		// Add status because we started matching ingress-class.
-		s.Logger.
-			WithField("name", newIng.GetName()).
-			WithField("namespace", newIng.GetNamespace()).
-			WithField("ingress-class", annotation.IngressClass(newIng)).
-			WithField("configured-ingress-class", s.IngressClass).
-			Debug("Updated Ingress is in scope, updating")
-		s.StatusUpdater.Update(
-			newIng.GetName(),
-			newIng.GetNamespace(),
-			v1beta1.SchemeGroupVersion.WithResource("ingresses"),
-			StatusMutatorFunc(ingressMutator(s.LBStatus)),
-		)
-	}
+	// We only care about the new object, because we're only updating its status.
+	// So, we can get away with just passing this call to OnAdd.
+	s.OnAdd(newObj)
 
-	// TODO(youngnick): There is a possibility that someone else may have edited the status, and we would then have
-	// no way to fix the object, because we're only operating on ingress-class change. After consideration, we've decided that
-	// editing the status subresource is hard enough that if someone does, they must have a reason. We can revisit if required.
-	// Checking annotation.MatchesIngressClass(newIng, s.IngressClass) && !reflect.DeepEqual(newIng.Status.Loadbalancer, s.LBStatus)
-	// would probably do it, but we have no way to verify for now.
 }
 
 func (s *StatusAddressUpdater) OnDelete(obj interface{}) {
 	// we don't need to update the status on resources that
 	// have been deleted.
-}
-
-func ingressMutator(lbstatus v1.LoadBalancerStatus) func(obj interface{}) interface{} {
-
-	return func(obj interface{}) interface{} {
-		ing := obj.(*v1beta1.Ingress).DeepCopy()
-		ing.Status.LoadBalancer = lbstatus
-		return ing
-	}
 }
 
 // ServiceStatusLoadBalancerWatcher implements ResourceEventHandler and
