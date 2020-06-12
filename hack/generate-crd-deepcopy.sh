327d33e930e5793f31957f2a53440e6dbac99c99
James Peach <jpeach@vmware.com>
2020-06-12 11:06:46 +1000

    Update and simplify the DeepCopy generator script. (#2571)
    Signed-off-by: James Peach <jpeach@vmware.com>

--- hack/generate-crd-deepcopy.sh
+++ hack/generate-crd-deepcopy.sh
@@ -1,19 +1,10 @@
 #! /usr/bin/env bash
 
-# See
-#	- https://github.com/kubernetes/sample-controller/blob/master/hack/update-codegen.sh
-#	- https://github.com/kubernetes/code-generator/blob/master/generate-groups.sh
-#
-# This script does basically the same as the generate-groups script linked
-# above. Since Go modules don't download scripts, don't have a vendored
-# copy of the script, hence this facsimilie.
-
 set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly REPO=$(cd $(dirname $0)/.. && pwd)
-readonly DESTDIR=${DESTDIR:-$(mktemp -d)}
+readonly REPO=$(cd "$(dirname "$0")/.." && pwd)
 readonly YEAR=$(date +%Y)
 
 readonly GO111MODULE=on
@@ -21,8 +12,6 @@ readonly GOFLAGS=-mod=vendor
 
 export GO111MODULE
 export GOFLAGS
-
-readonly CONTOUR="github.com/projectcontour/contour"
 
 
 boilerplate() {
@@ -45,35 +34,11 @@ limitations under the License.
 EOF
 }
 
-generator() {
-	local -r tool="$1"
+readonly HEADER=$(mktemp)
 
-	shift
+boilerplate > "${HEADER}"
 
-	go run k8s.io/code-generator/cmd/$tool \
-		--go-header-file ${DESTDIR}/boilerplate.go.tmpl \
-		--output-base ${DESTDIR} \
-		"$@"
-}
-
-# The output files will be generated to ${DESTDIR}/${CONTOUR}, so make a
-# symbolic link back to the current repository to make them update in place
-(
-	cd ${DESTDIR}
-	mkdir -p $(dirname $CONTOUR)
-	ln -s ${REPO} $(dirname $CONTOUR)/contour
-
-        boilerplate > ${DESTDIR}/boilerplate.go.tmpl
-)
-
-generator \
-deepcopy-gen \
-	--input-dirs ${CONTOUR}/apis/projectcontour/v1 \
-	--output-file-base zz_generated.deepcopy \
-	--output-package ${CONTOUR}/apis/projectcontour/v1
-
-generator \
-deepcopy-gen \
-	--input-dirs ${CONTOUR}/apis/contour/v1beta1 \
-	--output-file-base zz_generated.deepcopy \
-	--output-package ${CONTOUR}/apis/contour/v1beta1
+exec go run sigs.k8s.io/controller-tools/cmd/controller-gen \
+    object \
+    "object:headerFile=${HEADER}" \
+    paths=./apis/...
