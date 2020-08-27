f2210bdca6765ccee69658c95a153b4451c03e06
Steve Sloka <slokas@vmware.com>
2020-08-27 18:24:47 -0400

    cmd/contour: add xds-server-type arg to config file (#2840)
    Adds a config file option to define the XDS server to use
    when serving Envoy's gRPC XDS.
    
    Signed-off-by: Steve Sloka <slokas@vmware.com>

--- cmd/contour/servecontext.go
+++ cmd/contour/servecontext.go
@@ -45,8 +45,11 @@ type serveContext struct {
 	Kubeconfig string `yaml:"kubeconfig,omitempty"`
 
 	// contour's xds service parameters
-	xdsAddr                         string
-	xdsPort                         int
+	xdsAddr string
+	xdsPort int
+	// Defines the XDSServer to use for `contour serve`
+	// Defaults to "contour"
+	XDSServerType                   string `yaml:"xds-server-type,omitempty"`
 	caFile, contourCert, contourKey string
 
 	// contour's debug handler parameters
@@ -155,6 +158,7 @@ func newServeContext() *serveContext {
 		Kubeconfig:            filepath.Join(os.Getenv("HOME"), ".kube", "config"),
 		xdsAddr:               "127.0.0.1",
 		xdsPort:               8001,
+		XDSServerType:         "contour",
 		statsAddr:             "0.0.0.0",
 		statsPort:             8002,
 		debugAddr:             "127.0.0.1",
