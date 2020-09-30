bc8e3356b0a511d00a1026913094ad1e4b266df4
Tero Saarni <tero.saarni@est.tech>
2020-09-30 10:21:09 +0300

    internal: implement client authentication for backend TLS (#2910)
    * Added new parameter to configuration file to configure a name of a secret
      containing client certificate and private key.
    
    * Configure Envoy to authenticate itself towards upstream cluster with
      the client certificate when upstream connection uses TLS.
    
    * Initial version of integration test case (skipped by default)
    
    Signed-off-by: Tero Saarni <tero.saarni@est.tech>

--- internal/featuretests/backendcavalidation_test.go
+++ internal/featuretests/backendcavalidation_test.go
@@ -85,7 +85,7 @@ func TestClusterServiceTLSBackendCAValidation(t *testing.T) {
 	// assert that there is a regular, non validation enabled cluster in CDS.
 	c.Request(clusterType).Equals(&envoy_api_v2.DiscoveryResponse{
 		Resources: resources(t,
-			tlsCluster(cluster("default/kuard/443/da39a3ee5e", "default/kuard/securebackend", "default_kuard_443"), nil, "", ""),
+			tlsCluster(cluster("default/kuard/443/da39a3ee5e", "default/kuard/securebackend", "default_kuard_443"), nil, "", "", nil),
 		),
 		TypeUrl: clusterType,
 	})
@@ -133,7 +133,7 @@ func TestClusterServiceTLSBackendCAValidation(t *testing.T) {
 	// assert that the cluster now has a certificate and subject name.
 	c.Request(clusterType).Equals(&envoy_api_v2.DiscoveryResponse{
 		Resources: resources(t,
-			tlsCluster(cluster("default/kuard/443/98c0f31c72", "default/kuard/securebackend", "default_kuard_443"), []byte(CERTIFICATE), "subjname", ""),
+			tlsCluster(cluster("default/kuard/443/98c0f31c72", "default/kuard/securebackend", "default_kuard_443"), []byte(CERTIFICATE), "subjname", "", nil),
 		),
 		TypeUrl: clusterType,
 	})
@@ -190,7 +190,7 @@ func TestClusterServiceTLSBackendCAValidation(t *testing.T) {
 	// assert that the cluster now has a certificate and subject name.
 	c.Request(clusterType).Equals(&envoy_api_v2.DiscoveryResponse{
 		Resources: resources(t,
-			tlsCluster(cluster("default/kuard/443/98c0f31c72", "default/kuard/securebackend", "default_kuard_443"), []byte(CERTIFICATE), "subjname", ""),
+			tlsCluster(cluster("default/kuard/443/98c0f31c72", "default/kuard/securebackend", "default_kuard_443"), []byte(CERTIFICATE), "subjname", "", nil),
 		),
 		TypeUrl: clusterType,
 	})
