fc5a54561b654386a7ff6cee88ac4786fe9d86af
James Peach <jpeach@vmware.com>
2020-10-06 15:14:00 +1100

    internal/k8s: add a Contour scheme constructor
    Add a constructor to generate a scheme that includes all the
    Contour API types.
    
    This updates #2818.
    
    Signed-off-by: James Peach <jpeach@vmware.com>

---
+++ internal/k8s/scheme.go
@@ -0,0 +1,43 @@
+// Copyright Project Contour Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package k8s
+
+import (
+	contour_api_v1 "github.com/projectcontour/contour/apis/projectcontour/v1"
+	contour_api_v1alpha1 "github.com/projectcontour/contour/apis/projectcontour/v1alpha1"
+	"k8s.io/api/networking/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	serviceapis "sigs.k8s.io/service-apis/api/v1alpha1"
+)
+
+// NewContourScheme returns a scheme that includes all the API types
+// that Contour supports as well as the core Kubernetes API types from
+// the default scheme.
+func NewContourScheme() (*runtime.Scheme, error) {
+	s := runtime.NewScheme()
+	b := runtime.SchemeBuilder{
+		contour_api_v1.AddToScheme,
+		contour_api_v1alpha1.AddToScheme,
+		scheme.AddToScheme,
+		serviceapis.AddToScheme,
+		v1beta1.AddToScheme,
+	}
+
+	if err := b.AddToScheme(s); err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
