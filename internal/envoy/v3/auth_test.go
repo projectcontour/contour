6c8e20f1b80fc9994953cfeea7298afea1ad7564
Steve Sloka <slokas@vmware.com>
2020-11-10 15:34:42 -0500

    internal/envoy: Enable xDS v3 Clusters
    Enables v3 xDS Clusters as well as supporting structs.
    
    Updates #1898
    
    Signed-off-by: Steve Sloka <slokas@vmware.com>

---
+++ internal/envoy/v3/auth_test.go
@@ -0,0 +1,114 @@
+// Copyright Project Contour Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v3
+
+import (
+	"testing"
+
+	envoy_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	envoy_tls_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
+	matcher "github.com/envoyproxy/go-control-plane/envoy/type/matcher/v3"
+	"github.com/projectcontour/contour/internal/dag"
+	"github.com/projectcontour/contour/internal/protobuf"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestUpstreamTLSContext(t *testing.T) {
+	secret := &dag.Secret{
+		Object: &v1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "secret",
+				Namespace: "default",
+			},
+			Type: v1.SecretTypeTLS,
+			Data: map[string][]byte{dag.CACertificateKey: []byte("ca")},
+		},
+	}
+
+	tests := map[string]struct {
+		validation    *dag.PeerValidationContext
+		alpnProtocols []string
+		externalName  string
+		want          *envoy_tls_v3.UpstreamTlsContext
+	}{
+		"no alpn, no validation": {
+			want: &envoy_tls_v3.UpstreamTlsContext{
+				CommonTlsContext: &envoy_tls_v3.CommonTlsContext{},
+			},
+		},
+		"h2, no validation": {
+			alpnProtocols: []string{"h2c"},
+			want: &envoy_tls_v3.UpstreamTlsContext{
+				CommonTlsContext: &envoy_tls_v3.CommonTlsContext{
+					AlpnProtocols: []string{"h2c"},
+				},
+			},
+		},
+		"no alpn, missing altname": {
+			validation: &dag.PeerValidationContext{
+				CACertificate: secret,
+			},
+			want: &envoy_tls_v3.UpstreamTlsContext{
+				CommonTlsContext: &envoy_tls_v3.CommonTlsContext{},
+			},
+		},
+		"no alpn, missing ca": {
+			validation: &dag.PeerValidationContext{
+				SubjectName: "www.example.com",
+			},
+			want: &envoy_tls_v3.UpstreamTlsContext{
+				CommonTlsContext: &envoy_tls_v3.CommonTlsContext{},
+			},
+		},
+		"no alpn, ca and altname": {
+			validation: &dag.PeerValidationContext{
+				CACertificate: secret,
+				SubjectName:   "www.example.com",
+			},
+			want: &envoy_tls_v3.UpstreamTlsContext{
+				CommonTlsContext: &envoy_tls_v3.CommonTlsContext{
+					ValidationContextType: &envoy_tls_v3.CommonTlsContext_ValidationContext{
+						ValidationContext: &envoy_tls_v3.CertificateValidationContext{
+							TrustedCa: &envoy_core_v3.DataSource{
+								Specifier: &envoy_core_v3.DataSource_InlineBytes{
+									InlineBytes: []byte("ca"),
+								},
+							},
+							MatchSubjectAltNames: []*matcher.StringMatcher{{
+								MatchPattern: &matcher.StringMatcher_Exact{
+									Exact: "www.example.com",
+								}},
+							},
+						},
+					},
+				},
+			},
+		},
+		"external name sni": {
+			externalName: "projectcontour.local",
+			want: &envoy_tls_v3.UpstreamTlsContext{
+				CommonTlsContext: &envoy_tls_v3.CommonTlsContext{},
+				Sni:              "projectcontour.local",
+			},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := UpstreamTLSContext(tc.validation, tc.externalName, nil, tc.alpnProtocols...)
+			protobuf.ExpectEqual(t, tc.want, got)
+		})
+	}
+}
