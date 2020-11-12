c72650a36f3231a9aa483bd6d1edfd498383a974
Steve Sloka <slokas@vmware.com>
2020-11-12 14:22:16 -0500

    copy tls.go from envoy/v2 package into envoy/v3 package & upgrade to v3
    Signed-off-by: Steve Sloka <slokas@vmware.com>

---
+++ internal/envoy/v3/tls.go
@@ -0,0 +1,30 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v3
+
+import (
+	envoy_tls_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
+)
+
+func ParseTLSVersion(version string) envoy_tls_v3.TlsParameters_TlsProtocol {
+	switch version {
+	case "1.1":
+		return envoy_tls_v3.TlsParameters_TLSv1_1
+	case "1.2":
+		return envoy_tls_v3.TlsParameters_TLSv1_2
+	case "1.3":
+		return envoy_tls_v3.TlsParameters_TLSv1_3
+	default:
+		return envoy_tls_v3.TlsParameters_TLS_AUTO
+	}
+}
