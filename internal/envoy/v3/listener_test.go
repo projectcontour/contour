dcff4862d78abfe61d65dcdf1eedffc2608ecd25
Jesse Malone <jmalone@pivotal.io>
2021-02-08 17:32:39 -0500

    internal/envoy: Expose Envoy's delayed_close_timeout (#3316)
    There are situations where Envoy's "delayed_close_timeout"
    can close connections to the client even while there is data
    remaining to be written. This can happen when a client sets the
    "Connection: close" header and is slow to read the response.
    Users who encounter this may need to adjust the 'delayed_close_timeout'.
    
    This commit will expose delayed_close_timeout, making it
    configurable via the global contour configuration.
    
    Fixes #3285
    
    Signed-off-by: Jesse Malone <jmalone@vmware.com>
    Signed-off-by: iyacontrol <gaohj2015@yeah.net>

--- internal/envoy/v3/listener_test.go
+++ internal/envoy/v3/listener_test.go
@@ -327,6 +327,7 @@ func TestHTTPConnectionManager(t *testing.T) {
 		connectionIdleTimeout         timeout.Setting
 		streamIdleTimeout             timeout.Setting
 		maxConnectionDuration         timeout.Setting
+		delayedCloseTimeout           timeout.Setting
 		connectionShutdownGracePeriod timeout.Setting
 		allowChunkedLength            bool
 		xffNumTrustedHops             uint32
@@ -865,6 +866,95 @@ func TestHTTPConnectionManager(t *testing.T) {
 				},
 			},
 		},
+		"delayed close timeout of 90s": {
+			routename:           "default/kuard",
+			accesslogger:        FileAccessLogEnvoy("/dev/stdout"),
+			delayedCloseTimeout: timeout.DurationSetting(90 * time.Second),
+			want: &envoy_listener_v3.Filter{
+				Name: wellknown.HTTPConnectionManager,
+				ConfigType: &envoy_listener_v3.Filter_TypedConfig{
+					TypedConfig: protobuf.MustMarshalAny(&http.HttpConnectionManager{
+						StatPrefix: "default/kuard",
+						RouteSpecifier: &http.HttpConnectionManager_Rds{
+							Rds: &http.Rds{
+								RouteConfigName: "default/kuard",
+								ConfigSource: &envoy_core_v3.ConfigSource{
+									ResourceApiVersion: envoy_core_v3.ApiVersion_V3,
+									ConfigSourceSpecifier: &envoy_core_v3.ConfigSource_ApiConfigSource{
+										ApiConfigSource: &envoy_core_v3.ApiConfigSource{
+											ApiType:             envoy_core_v3.ApiConfigSource_GRPC,
+											TransportApiVersion: envoy_core_v3.ApiVersion_V3,
+											GrpcServices: []*envoy_core_v3.GrpcService{{
+												TargetSpecifier: &envoy_core_v3.GrpcService_EnvoyGrpc_{
+													EnvoyGrpc: &envoy_core_v3.GrpcService_EnvoyGrpc{
+														ClusterName: "contour",
+													},
+												},
+											}},
+										},
+									},
+								},
+							},
+						},
+						HttpFilters: []*http.HttpFilter{{
+							Name: "compressor",
+							ConfigType: &http.HttpFilter_TypedConfig{
+								TypedConfig: protobuf.MustMarshalAny(&envoy_compressor_v3.Compressor{
+									CompressorLibrary: &envoy_core_v3.TypedExtensionConfig{
+										Name: "gzip",
+										TypedConfig: &any.Any{
+											TypeUrl: HTTPFilterGzip,
+										},
+									},
+								}),
+							},
+						}, {
+							Name: "grpcweb",
+							ConfigType: &http.HttpFilter_TypedConfig{
+								TypedConfig: &any.Any{
+									TypeUrl: HTTPFilterGrpcWeb,
+								},
+							},
+						}, {
+							Name: "cors",
+							ConfigType: &http.HttpFilter_TypedConfig{
+								TypedConfig: &any.Any{
+									TypeUrl: HTTPFilterCORS,
+								},
+							},
+						}, {
+							Name: "local_ratelimit",
+							ConfigType: &http.HttpFilter_TypedConfig{
+								TypedConfig: protobuf.MustMarshalAny(
+									&envoy_config_filter_http_local_ratelimit_v3.LocalRateLimit{
+										StatPrefix: "http",
+									},
+								),
+							},
+						}, {
+							Name: "router",
+							ConfigType: &http.HttpFilter_TypedConfig{
+								TypedConfig: &any.Any{
+									TypeUrl: HTTPFilterRouter,
+								},
+							},
+						}},
+						HttpProtocolOptions: &envoy_core_v3.Http1ProtocolOptions{
+							// Enable support for HTTP/1.0 requests that carry
+							// a Host: header. See #537.
+							AcceptHttp_10: true,
+						},
+						CommonHttpProtocolOptions: &envoy_core_v3.HttpProtocolOptions{},
+						AccessLog:                 FileAccessLogEnvoy("/dev/stdout"),
+						UseRemoteAddress:          protobuf.Bool(true),
+						NormalizePath:             protobuf.Bool(true),
+						PreserveExternalRequestId: true,
+						MergeSlashes:              true,
+						DelayedCloseTimeout:       protobuf.Duration(90 * time.Second),
+					}),
+				},
+			},
+		},
 		"drain timeout of 90s": {
 			routename:                     "default/kuard",
 			accesslogger:                  FileAccessLogEnvoy("/dev/stdout"),
@@ -1147,6 +1237,7 @@ func TestHTTPConnectionManager(t *testing.T) {
 				ConnectionIdleTimeout(tc.connectionIdleTimeout).
 				StreamIdleTimeout(tc.streamIdleTimeout).
 				MaxConnectionDuration(tc.maxConnectionDuration).
+				DelayedCloseTimeout(tc.delayedCloseTimeout).
 				ConnectionShutdownGracePeriod(tc.connectionShutdownGracePeriod).
 				AllowChunkedLength(tc.allowChunkedLength).
 				NumTrustedHops(tc.xffNumTrustedHops).
