33b5a4928e09007d32a0e92a2db06c784d6b7d90
Jonas Rosland <jrosland@vmware.com>
2021-03-16 13:56:42 -0400

    final fixes
    Signed-off-by: Jonas Rosland <jrosland@vmware.com>

--- site/themes/contour/static/js/main.js
+++ site/themes/contour/static/js/main.js
@@ -9,3 +9,14 @@ function docsVersionToggle() {
     var menu = document.getElementById("dropdown-menu");
     menu.classList.toggle('dropdown-menu-visible');
 }
+
+window.onclick = function(event) {
+    var 
+        target = event.target,
+        menu = document.getElementById("dropdown-menu")
+    ;
+
+    if(!target.classList.contains('dropdown-toggle')) {
+        menu.classList.remove('dropdown-menu-visible');
+    }
+}
