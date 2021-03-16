6b1b60afeba50359089ddbbe533f03e9d4aeea99
Jonas Rosland <jrosland@vmware.com>
2021-03-16 13:56:42 -0400

    fix quotes
    Signed-off-by: Jonas Rosland <jrosland@vmware.com>

--- site/themes/contour/static/js/main.js
+++ site/themes/contour/static/js/main.js
@@ -1,19 +1,19 @@
 "use strict";
 
 function mobileNavToggle() {
-    var menu = document.getElementById("mobile-menu").parentElement;
+    var menu = document.getElementById('mobile-menu').parentElement;
     menu.classList.toggle('mobile-menu-visible');
 }
 
 function docsVersionToggle() {
-    var menu = document.getElementById("dropdown-menu");
+    var menu = document.getElementById('dropdown-menu');
     menu.classList.toggle('dropdown-menu-visible');
 }
 
 window.onclick = function(event) {
     var 
         target = event.target,
-        menu = document.getElementById("dropdown-menu")
+        menu = document.getElementById('dropdown-menu')
     ;
 
     if(!target.classList.contains('dropdown-toggle')) {
