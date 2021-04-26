5f1cc94b98579d838a0a88e4fc91447388fd8337
Steve Sloka <slokas@vmware.com>
2021-04-26 16:16:10 -0400

    internal/dag: Filter on Gateway.Listener.Protocol for supported types (#3612)
    * internal/dag: Filter on Gateway.Listener.Protocol for supported types
    
    Filters the Protocol on gateway listeners for only valid types (e.g. HTTP, HTTPS, TLS).
    
    Fixes #3540
    
    Signed-off-by: Steve Sloka <slokas@vmware.com>

--- internal/dag/builder_test.go
+++ internal/dag/builder_test.go
@@ -1159,6 +1159,73 @@ func TestDAGInsertGatewayAPI(t *testing.T) {
 				},
 			),
 		},
+		"insert basic single route, single hostname, gateway with TLS, HTTP protocol is ignored": {
+			gateway: &gatewayapi_v1alpha1.Gateway{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "contour",
+					Namespace: "projectcontour",
+				},
+				Spec: gatewayapi_v1alpha1.GatewaySpec{
+					Listeners: []gatewayapi_v1alpha1.Listener{{
+						Port:     443,
+						Protocol: gatewayapi_v1alpha1.HTTPProtocolType,
+						TLS: &gatewayapi_v1alpha1.GatewayTLSConfig{
+							CertificateRef: &gatewayapi_v1alpha1.LocalObjectReference{
+								Group: "core",
+								Kind:  "Secret",
+								Name:  sec1.Name,
+							},
+						},
+						Routes: gatewayapi_v1alpha1.RouteBindingSelector{
+							Kind: KindHTTPRoute,
+							Namespaces: gatewayapi_v1alpha1.RouteNamespaces{
+								From: gatewayapi_v1alpha1.RouteSelectAll,
+							},
+						},
+					}},
+				},
+			},
+			objs: []interface{}{
+				sec1,
+				kuardService,
+				genericHTTPRoute,
+			},
+			want: listeners(
+				&Listener{
+					Port: 80,
+					VirtualHosts: virtualhosts(
+						virtualhost("test.projectcontour.io",
+							prefixrouteHTTPRoute("/", service(kuardService)),
+						)),
+				},
+			),
+		},
+		"insert basic single route, single hostname, gateway with TLS, HTTPS protocol missing certificateRef": {
+			gateway: &gatewayapi_v1alpha1.Gateway{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "contour",
+					Namespace: "projectcontour",
+				},
+				Spec: gatewayapi_v1alpha1.GatewaySpec{
+					Listeners: []gatewayapi_v1alpha1.Listener{{
+						Port:     443,
+						Protocol: gatewayapi_v1alpha1.HTTPSProtocolType,
+						Routes: gatewayapi_v1alpha1.RouteBindingSelector{
+							Kind: KindHTTPRoute,
+							Namespaces: gatewayapi_v1alpha1.RouteNamespaces{
+								From: gatewayapi_v1alpha1.RouteSelectAll,
+							},
+						},
+					}},
+				},
+			},
+			objs: []interface{}{
+				sec1,
+				kuardService,
+				genericHTTPRoute,
+			},
+			want: listeners(),
+		},
 		"insert basic single route, single hostname, gateway with TLS": {
 			gateway: gatewayWithOnlyTLS,
 			objs: []interface{}{
@@ -1234,39 +1301,6 @@ func TestDAGInsertGatewayAPI(t *testing.T) {
 					),
 				},
 			),
-		},
-		"Only the Spec.Listener.Protocol: HTTPS should be valid for a TLS Listener Gateway": {
-			gateway: &gatewayapi_v1alpha1.Gateway{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "contour",
-					Namespace: "projectcontour",
-				},
-				Spec: gatewayapi_v1alpha1.GatewaySpec{
-					Listeners: []gatewayapi_v1alpha1.Listener{{
-						Port:     443,
-						Protocol: gatewayapi_v1alpha1.HTTPProtocolType,
-						TLS: &gatewayapi_v1alpha1.GatewayTLSConfig{
-							CertificateRef: &gatewayapi_v1alpha1.LocalObjectReference{
-								Group: "core",
-								Kind:  "Secret",
-								Name:  sec1.Name,
-							},
-						},
-						Routes: gatewayapi_v1alpha1.RouteBindingSelector{
-							Kind: KindHTTPRoute,
-							Namespaces: gatewayapi_v1alpha1.RouteNamespaces{
-								From: gatewayapi_v1alpha1.RouteSelectAll,
-							},
-						},
-					}},
-				},
-			},
-			objs: []interface{}{
-				sec1,
-				blogService,
-				httpRouteProtocolHTTPS,
-			},
-			want: listeners(),
 		},
 		"TLS Listener Gateway CertificateRef must be type core.Secret": {
 			gateway: &gatewayapi_v1alpha1.Gateway{
@@ -1352,6 +1386,72 @@ func TestDAGInsertGatewayAPI(t *testing.T) {
 					},
 				},
 			},
+			want: listeners(),
+		},
+		"Invalid listener protocol type (TCP)": {
+			gateway: &gatewayapi_v1alpha1.Gateway{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "contour",
+					Namespace: "projectcontour",
+				},
+				Spec: gatewayapi_v1alpha1.GatewaySpec{
+					Listeners: []gatewayapi_v1alpha1.Listener{{
+						Port:     80,
+						Protocol: gatewayapi_v1alpha1.TCPProtocolType,
+						Routes: gatewayapi_v1alpha1.RouteBindingSelector{
+							Kind: KindHTTPRoute,
+							Namespaces: gatewayapi_v1alpha1.RouteNamespaces{
+								From: gatewayapi_v1alpha1.RouteSelectAll,
+							},
+						},
+					}},
+				},
+			},
+			objs: []interface{}{genericHTTPRoute},
+			want: listeners(),
+		},
+		"Invalid listener protocol type (UDP)": {
+			gateway: &gatewayapi_v1alpha1.Gateway{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "contour",
+					Namespace: "projectcontour",
+				},
+				Spec: gatewayapi_v1alpha1.GatewaySpec{
+					Listeners: []gatewayapi_v1alpha1.Listener{{
+						Port:     80,
+						Protocol: gatewayapi_v1alpha1.UDPProtocolType,
+						Routes: gatewayapi_v1alpha1.RouteBindingSelector{
+							Kind: KindHTTPRoute,
+							Namespaces: gatewayapi_v1alpha1.RouteNamespaces{
+								From: gatewayapi_v1alpha1.RouteSelectAll,
+							},
+						},
+					}},
+				},
+			},
+			objs: []interface{}{genericHTTPRoute},
+			want: listeners(),
+		},
+		"Invalid listener protocol type (custom)": {
+			gateway: &gatewayapi_v1alpha1.Gateway{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "contour",
+					Namespace: "projectcontour",
+				},
+				Spec: gatewayapi_v1alpha1.GatewaySpec{
+					Listeners: []gatewayapi_v1alpha1.Listener{{
+						Port:     80,
+						Protocol: "projectcontour.io/HTTPUDP",
+						Routes: gatewayapi_v1alpha1.RouteBindingSelector{
+							Kind: KindHTTPRoute,
+							Namespaces: gatewayapi_v1alpha1.RouteNamespaces{
+								From: gatewayapi_v1alpha1.RouteSelectAll,
+							},
+						},
+					}},
+				},
+			},
+			objs: []interface{}{genericHTTPRoute},
 			want: listeners(),
 		},
 		"insert basic single route, single hostname, gateway with TLS & Insecure Listeners, different selectors": {
