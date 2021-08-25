8a36305c1c20af3825c467676dcdf43c211e1266
Sunjay Bhatia <5337253+sunjayBhatia@users.noreply.github.com>
2021-08-25 15:16:23 -0400

    Cherrypick #3934 (#3982)
    Move to Envoy Admin over unix socket to mitigate security issues
    with external name services.
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>
    
    Co-authored-by: Steve Sloka <slokas@vmware.com>

--- internal/envoy/v3/listener.go
+++ internal/envoy/v3/listener.go
@@ -508,6 +508,18 @@ func TCPProxy(statPrefix string, proxy *dag.TCPProxy, accesslogger []*accesslog.
 	}
 }
 
+// UnixSocketAddress creates a new Unix Socket envoy_core_v3.Address.
+func UnixSocketAddress(address string, port int) *envoy_core_v3.Address {
+	return &envoy_core_v3.Address{
+		Address: &envoy_core_v3.Address_Pipe{
+			Pipe: &envoy_core_v3.Pipe{
+				Path: address,
+				Mode: 0644,
+			},
+		},
+	}
+}
+
 // SocketAddress creates a new TCP envoy_core_v3.Address.
 func SocketAddress(address string, port int) *envoy_core_v3.Address {
 	if address == "::" {
