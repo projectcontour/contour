0b7ec5c3bac9ff316f1281073d23c83163896374
Sunjay Bhatia <sunjayb@vmware.com>
2021-11-11 19:21:07 +0000

    Get kind network subnet
    So we can use that in multinode setup config for metallb
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>

--- test/scripts/make-kind-cluster.sh
+++ test/scripts/make-kind-cluster.sh
@@ -85,9 +85,30 @@ done
 if [[ "${MULTINODE_CLUSTER}" == "true" ]]; then
     # Install metallb.
     ${KUBECTL} apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/namespace.yaml
-    ${KUBECTL} create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+    if ! kubectl get secret -n metallb-system memberlist; then
+        ${KUBECTL} create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+    fi
     ${KUBECTL} apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/metallb.yaml
     ${KUBECTL} apply -f https://kind.sigs.k8s.io/examples/loadbalancer/metallb-configmap.yaml
+    # Apply config with addresses based on docker network IPAM
+    subnet=$(docker network inspect kind | jq -r '.[].IPAM.Config[].Subnet | select(contains(":") | not)')
+    # Assume default kind network subnet prefix of 16, and choose addresses in that range.
+    address_first_octets=$(echo ${subnet} | awk -F. '{printf "%s.%s",$1,$2}')
+    address_range="${address_first_octets}.255.200-${address_first_octets}.255.250"
+    ${KUBECTL} apply -f - <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - ${address_range}
+EOF
 fi
 
 # Install cert-manager.
