e445e3823577c12597e4ed56250395cddf84b211
Steve Kriss <krisss@vmware.com>
2022-01-21 18:57:03 +0000

    code review
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- cmd/contour/serve.go
+++ cmd/contour/serve.go
@@ -389,22 +389,16 @@ func (s *Server) doServe() error {
 		return err
 	}
 
-	var applyHeaderPolicyToIngress bool
-	if contourConfiguration.Policy != nil {
-		applyHeaderPolicyToIngress = contourConfiguration.Policy.ApplyToIngress
-	}
-
 	builder := s.getDAGBuilder(dagBuilderConfig{
-		ingressClassName:           ingressClassName,
-		rootNamespaces:             contourConfiguration.HTTPProxy.RootNamespaces,
-		gatewayAPIConfigured:       contourConfiguration.Gateway != nil,
-		disablePermitInsecure:      contourConfiguration.HTTPProxy.DisablePermitInsecure,
-		enableExternalNameService:  contourConfiguration.EnableExternalNameService,
-		dnsLookupFamily:            contourConfiguration.Envoy.Cluster.DNSLookupFamily,
-		headersPolicy:              contourConfiguration.Policy,
-		applyHeaderPolicyToIngress: applyHeaderPolicyToIngress,
-		clientCert:                 clientCert,
-		fallbackCert:               fallbackCert,
+		ingressClassName:          ingressClassName,
+		rootNamespaces:            contourConfiguration.HTTPProxy.RootNamespaces,
+		gatewayAPIConfigured:      contourConfiguration.Gateway != nil,
+		disablePermitInsecure:     contourConfiguration.HTTPProxy.DisablePermitInsecure,
+		enableExternalNameService: contourConfiguration.EnableExternalNameService,
+		dnsLookupFamily:           contourConfiguration.Envoy.Cluster.DNSLookupFamily,
+		headersPolicy:             contourConfiguration.Policy,
+		clientCert:                clientCert,
+		fallbackCert:              fallbackCert,
 	})
 
 	// Build the core Kubernetes event handler.
@@ -776,22 +770,24 @@ func (s *Server) setupGatewayAPI(contourConfiguration contour_api_v1alpha1.Conto
 }
 
 type dagBuilderConfig struct {
-	ingressClassName           string
-	rootNamespaces             []string
-	gatewayAPIConfigured       bool
-	disablePermitInsecure      bool
-	enableExternalNameService  bool
-	dnsLookupFamily            contour_api_v1alpha1.ClusterDNSFamilyType
-	headersPolicy              *contour_api_v1alpha1.PolicyConfig
-	applyHeaderPolicyToIngress bool
-	clientCert                 *types.NamespacedName
-	fallbackCert               *types.NamespacedName
+	ingressClassName          string
+	rootNamespaces            []string
+	gatewayAPIConfigured      bool
+	disablePermitInsecure     bool
+	enableExternalNameService bool
+	dnsLookupFamily           contour_api_v1alpha1.ClusterDNSFamilyType
+	headersPolicy             *contour_api_v1alpha1.PolicyConfig
+	clientCert                *types.NamespacedName
+	fallbackCert              *types.NamespacedName
 }
 
 func (s *Server) getDAGBuilder(dbc dagBuilderConfig) *dag.Builder {
 
-	var requestHeadersPolicy dag.HeadersPolicy
-	var responseHeadersPolicy dag.HeadersPolicy
+	var (
+		requestHeadersPolicy       dag.HeadersPolicy
+		responseHeadersPolicy      dag.HeadersPolicy
+		applyHeaderPolicyToIngress bool
+	)
 
 	if dbc.headersPolicy != nil {
 		if dbc.headersPolicy.RequestHeadersPolicy != nil {
@@ -819,11 +815,14 @@ func (s *Server) getDAGBuilder(dbc dagBuilderConfig) *dag.Builder {
 				responseHeadersPolicy.Remove = append(responseHeadersPolicy.Remove, dbc.headersPolicy.ResponseHeadersPolicy.Remove...)
 			}
 		}
+
+		applyHeaderPolicyToIngress = dbc.headersPolicy.ApplyToIngress
 	}
 
 	var requestHeadersPolicyIngress dag.HeadersPolicy
 	var responseHeadersPolicyIngress dag.HeadersPolicy
-	if dbc.applyHeaderPolicyToIngress {
+
+	if applyHeaderPolicyToIngress {
 		requestHeadersPolicyIngress = requestHeadersPolicy
 		responseHeadersPolicyIngress = responseHeadersPolicy
 	}
