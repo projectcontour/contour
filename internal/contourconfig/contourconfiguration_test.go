d4f05df85723d16181a5d80ca64bddfa49d51e00
Steve Kriss <krisss@vmware.com>
2022-04-07 02:40:36 +0000

    improve testing of OverlayOnDefaults
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- internal/contourconfig/contourconfiguration_test.go
+++ internal/contourconfig/contourconfiguration_test.go
@@ -26,25 +26,186 @@ import (
 )
 
 func TestOverlayOnDefaults(t *testing.T) {
-	// An empty ContourConfig results in just the defaults.
-	cfg := contour_api_v1alpha1.ContourConfigurationSpec{}
-	res, err := contourconfig.OverlayOnDefaults(cfg)
-	require.NoError(t, err)
-	assert.Equal(t, contourconfig.Defaults(), res)
-
-	// A single non-default field is overlaid correctly.
-	cfg = contour_api_v1alpha1.ContourConfigurationSpec{
+	allFieldsSpecified := contour_api_v1alpha1.ContourConfigurationSpec{
 		XDSServer: &contour_api_v1alpha1.XDSServerConfig{
-			Type: contour_api_v1alpha1.EnvoyServerType,
+			Type:    contour_api_v1alpha1.EnvoyServerType,
+			Address: "7.7.7.7",
+			Port:    7777,
+			TLS: &contour_api_v1alpha1.TLS{
+				CAFile:   "/foo/ca.crt",
+				CertFile: "/foo/tls.crt",
+				KeyFile:  "/foo/tls.key",
+				Insecure: pointer.Bool(true),
+			},
+		},
+		Ingress: &contour_api_v1alpha1.IngressConfig{
+			ClassNames:    []string{"coolclass"},
+			StatusAddress: "7.7.7.7",
+		},
+		Debug: &contour_api_v1alpha1.DebugConfig{
+			Address:                 "1.2.3.4",
+			Port:                    6789,
+			DebugLogLevel:           contour_api_v1alpha1.DebugLog,
+			KubernetesDebugLogLevel: contourconfig.UIntPtr(7),
+		},
+		Health: &contour_api_v1alpha1.HealthConfig{
+			Address: "2.3.4.5",
+			Port:    8888,
+		},
+		Envoy: &contour_api_v1alpha1.EnvoyConfig{
+			Listener: &contour_api_v1alpha1.EnvoyListenerConfig{
+				UseProxyProto:             pointer.Bool(true),
+				DisableAllowChunkedLength: pointer.Bool(true),
+				DisableMergeSlashes:       pointer.Bool(true),
+				ConnectionBalancer:        "yesplease",
+				TLS: &contour_api_v1alpha1.EnvoyTLS{
+					MinimumProtocolVersion: "1.7",
+					CipherSuites: []contour_api_v1alpha1.TLSCipherType{
+						"foo",
+						"bar",
+					},
+				},
+			},
+			Service: &contour_api_v1alpha1.NamespacedName{
+				Namespace: "coolnamespace",
+				Name:      "coolname",
+			},
+			HTTPListener: &contour_api_v1alpha1.EnvoyListener{
+				Address:   "3.4.5.6",
+				Port:      8989,
+				AccessLog: "/dev/oops",
+			},
+			HTTPSListener: &contour_api_v1alpha1.EnvoyListener{
+				Address:   "4.5.6.7",
+				Port:      8445,
+				AccessLog: "/dev/oops",
+			},
+			Health: &contour_api_v1alpha1.HealthConfig{
+				Address: "1.1.1.1",
+				Port:    8222,
+			},
+			Metrics: &contour_api_v1alpha1.MetricsConfig{
+				Address: "1.2.12.1212",
+				Port:    8882,
+				TLS: &contour_api_v1alpha1.MetricsTLS{
+					CAFile:   "cafile",
+					CertFile: "certfile",
+					KeyFile:  "keyfile",
+				},
+			},
+			ClientCertificate: &contour_api_v1alpha1.NamespacedName{
+				Namespace: "clientcertnamespace",
+				Name:      "clientcertname",
+			},
+			Logging: &contour_api_v1alpha1.EnvoyLogging{
+				AccessLogFormat:       contour_api_v1alpha1.JSONAccessLog,
+				AccessLogFormatString: "foo",
+				AccessLogFields:       []string{"field-1", "field-2"},
+				AccessLogLevel:        contour_api_v1alpha1.LogLevelError,
+			},
+			DefaultHTTPVersions: []contour_api_v1alpha1.HTTPVersionType{
+				"HTTP/2.2",
+				"HTTP/3",
+			},
+			Timeouts: &contour_api_v1alpha1.TimeoutParameters{
+				RequestTimeout:                pointer.String("1s"),
+				ConnectionIdleTimeout:         pointer.String("2s"),
+				StreamIdleTimeout:             pointer.String("3s"),
+				MaxConnectionDuration:         pointer.String("4s"),
+				DelayedCloseTimeout:           pointer.String("5s"),
+				ConnectionShutdownGracePeriod: pointer.String("6s"),
+				ConnectTimeout:                pointer.String("7s"),
+			},
+			Cluster: &contour_api_v1alpha1.ClusterParameters{
+				DNSLookupFamily: contour_api_v1alpha1.IPv4ClusterDNSFamily,
+			},
+			Network: &contour_api_v1alpha1.NetworkParameters{
+				XffNumTrustedHops: contourconfig.UInt32Ptr(77),
+				EnvoyAdminPort:    pointer.Int(9997),
+			},
+		},
+		Gateway: &contour_api_v1alpha1.GatewayConfig{
+			ControllerName: "gatewaycontroller",
+			GatewayRef: &contour_api_v1alpha1.NamespacedName{
+				Namespace: "gatewaynamespace",
+				Name:      "gatewayname",
+			},
+		},
+		HTTPProxy: &contour_api_v1alpha1.HTTPProxyConfig{
+			DisablePermitInsecure: pointer.Bool(true),
+			RootNamespaces:        []string{"rootnamespace"},
+			FallbackCertificate: &contour_api_v1alpha1.NamespacedName{
+				Namespace: "fallbackcertificatenamespace",
+				Name:      "fallbackcertificatename",
+			},
+		},
+		EnableExternalNameService: pointer.Bool(true),
+		RateLimitService: &contour_api_v1alpha1.RateLimitServiceConfig{
+			ExtensionService: contour_api_v1alpha1.NamespacedName{
+				Namespace: "ratelimitservicenamespace",
+				Name:      "ratelimitservicename",
+			},
+			Domain:                  "ratelimitservicedomain",
+			FailOpen:                pointer.Bool(true),
+			EnableXRateLimitHeaders: pointer.Bool(true),
+		},
+		Policy: &contour_api_v1alpha1.PolicyConfig{
+			RequestHeadersPolicy: &contour_api_v1alpha1.HeadersPolicy{
+				Set:    map[string]string{"set": "val"},
+				Remove: []string{"remove"},
+			},
+			ResponseHeadersPolicy: &contour_api_v1alpha1.HeadersPolicy{
+				Set:    map[string]string{"set": "val"},
+				Remove: []string{"remove"},
+			},
+			ApplyToIngress: pointer.Bool(true),
+		},
+		Metrics: &contour_api_v1alpha1.MetricsConfig{
+			Address: "9.8.7.6",
+			Port:    9876,
+			TLS: &contour_api_v1alpha1.MetricsTLS{
+				CAFile:   "cafile.cafile",
+				CertFile: "certfile.certfile",
+				KeyFile:  "keyfile.keyfile",
+			},
 		},
 	}
 
-	want := contourconfig.Defaults()
-	want.XDSServer.Type = contour_api_v1alpha1.EnvoyServerType
+	tests := map[string]struct {
+		contourConfig contour_api_v1alpha1.ContourConfigurationSpec
+		want          func() contour_api_v1alpha1.ContourConfigurationSpec
+	}{
+		"empty ContourConfig results in all the defaults": {
+			contourConfig: contour_api_v1alpha1.ContourConfigurationSpec{},
+			want:          contourconfig.Defaults,
+		},
+		"ContourConfig with single non-default field is overlaid correctly": {
+			contourConfig: contour_api_v1alpha1.ContourConfigurationSpec{
+				XDSServer: &contour_api_v1alpha1.XDSServerConfig{
+					Type: contour_api_v1alpha1.EnvoyServerType,
+				},
+			},
+			want: func() contour_api_v1alpha1.ContourConfigurationSpec {
+				res := contourconfig.Defaults()
+				res.XDSServer.Type = contour_api_v1alpha1.EnvoyServerType
+				return res
+			},
+		},
+		"ContourConfig with every field specified with a non-default value results in all of those values used": {
+			contourConfig: allFieldsSpecified,
+			want: func() contour_api_v1alpha1.ContourConfigurationSpec {
+				return allFieldsSpecified
+			},
+		},
+	}
 
-	res, err = contourconfig.OverlayOnDefaults(cfg)
-	require.NoError(t, err)
-	assert.Equal(t, want, res)
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			res, err := contourconfig.OverlayOnDefaults(tc.contourConfig)
+			require.NoError(t, err)
+			assert.Equal(t, tc.want(), res)
+		})
+	}
 }
 
 func TestParseTimeoutPolicy(t *testing.T) {
