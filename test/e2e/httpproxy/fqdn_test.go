bbb5aa06f61130876e75c6a321035d6ca40da41d
Sunjay Bhatia <5337253+sunjayBhatia@users.noreply.github.com>
2022-04-15 14:25:01 -0400

    Add test and documentation to show precise hostnames override wildcards (#4463)
    Closes #4128
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>

--- test/e2e/httpproxy/fqdn_test.go
+++ test/e2e/httpproxy/fqdn_test.go
@@ -17,11 +17,14 @@
 package httpproxy
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo/v2"
 	contourv1 "github.com/projectcontour/contour/apis/projectcontour/v1"
 	"github.com/projectcontour/contour/test/e2e"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -124,7 +127,10 @@ func testWildcardSubdomainFQDN(namespace string) {
 		f.CreateHTTPProxyAndWaitFor(proxyFullFQDN, e2e.HTTPProxyValid)
 		f.CreateHTTPProxyAndWaitFor(proxyFullFQDNSubdomain, e2e.HTTPProxyValid)
 
-		cases := map[string]ServiceResult{
+		cases := map[string]struct {
+			ServiceName   string
+			ShouldSucceed bool
+		}{
 			"domain.io": {
 				ServiceName:   "domainio",
 				ShouldSucceed: true,
@@ -176,7 +182,87 @@ func testWildcardSubdomainFQDN(namespace string) {
 	})
 }
 
-type ServiceResult struct {
-	ServiceName   string
-	ShouldSucceed bool
+func testIngressWildcardSubdomainFQDN(namespace string) {
+	Specify("wildcard from an Ingress is overridden by a HTTPProxy with subdomain", func() {
+		t := f.T()
+
+		f.Fixtures.Echo.Deploy(namespace, "wildcarddomainio")
+		f.Fixtures.Echo.Deploy(namespace, "bardomainio")
+
+		ingressWildcard := &networkingv1.Ingress{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: namespace,
+				Name:      "wildcard-ingress",
+			},
+			Spec: networkingv1.IngressSpec{
+				Rules: []networkingv1.IngressRule{
+					{
+						Host: "*.wildcard-override.projectcontour.io",
+						IngressRuleValue: networkingv1.IngressRuleValue{
+							HTTP: &networkingv1.HTTPIngressRuleValue{
+								Paths: []networkingv1.HTTPIngressPath{
+									{
+										PathType: e2e.IngressPathTypePtr(networkingv1.PathTypePrefix),
+										Path:     "/",
+										Backend: networkingv1.IngressBackend{
+											Service: &networkingv1.IngressServiceBackend{
+												Name: "wildcarddomainio",
+												Port: networkingv1.ServiceBackendPort{
+													Number: 80,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		require.NoError(t, f.Client.Create(context.TODO(), ingressWildcard))
+
+		proxySubdomain := &contourv1.HTTPProxy{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: namespace,
+				Name:      "fqdn-subdomain",
+			},
+			Spec: contourv1.HTTPProxySpec{
+				VirtualHost: &contourv1.VirtualHost{
+					Fqdn: "bar.wildcard-override.projectcontour.io",
+				},
+				Routes: []contourv1.Route{{
+					Services: []contourv1.Service{
+						{
+							Name: "bardomainio",
+							Port: 80,
+						},
+					},
+				}},
+			},
+		}
+		f.CreateHTTPProxyAndWaitFor(proxySubdomain, e2e.HTTPProxyValid)
+
+		res, ok := f.HTTP.RequestUntil(&e2e.HTTPRequestOpts{
+			Host:      "www.wildcard-override.projectcontour.io",
+			Path:      "/",
+			Condition: e2e.HasStatusCode(200),
+		})
+		require.NotNil(t, res, "request never succeeded")
+		assert.Truef(t, ok, "expected 200 response code, got %d", res.StatusCode)
+		body := f.GetEchoResponseBody(res.Body)
+		assert.Equal(t, namespace, body.Namespace)
+		assert.Equal(t, "wildcarddomainio", body.Service)
+
+		res, ok = f.HTTP.RequestUntil(&e2e.HTTPRequestOpts{
+			Host:      "bar.wildcard-override.projectcontour.io",
+			Path:      "/",
+			Condition: e2e.HasStatusCode(200),
+		})
+		require.NotNil(t, res, "request never succeeded")
+		assert.Truef(t, ok, "expected 200 response code, got %d", res.StatusCode)
+		body = f.GetEchoResponseBody(res.Body)
+		assert.Equal(t, namespace, body.Namespace)
+		assert.Equal(t, "bardomainio", body.Service)
+	})
 }
