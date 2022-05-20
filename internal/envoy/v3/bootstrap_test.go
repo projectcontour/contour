3fe757c9c42f301c0839b2523dee67f0a1b78dfe
Sunjay Bhatia <5337253+sunjayBhatia@users.noreply.github.com>
2022-05-20 16:06:25 -0400

    internal/envoy/v3: Use path_config_source for config source specifier (#4535)
    "path" config source is deprecated for path_config_source which also
    adds the option of a watched directory that can be for different uses
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>

--- internal/envoy/v3/bootstrap_test.go
+++ internal/envoy/v3/bootstrap_test.go
@@ -1406,7 +1406,9 @@ func TestBootstrap(t *testing.T) {
                         "name": "contour_xds_tls_certificate",
                         "sds_config": {
                           "resource_api_version": "V3",
-                          "path": "resources/sds/xds-tls-certificate.json"
+                          "path_config_source": {
+                            "path": "resources/sds/xds-tls-certificate.json"
+                          }
                         }
                       }
                     ],
@@ -1414,7 +1416,9 @@ func TestBootstrap(t *testing.T) {
                       "name": "contour_xds_tls_validation_context",
                       "sds_config": {
                         "resource_api_version": "V3",
-                        "path": "resources/sds/xds-validation-context.json"
+                        "path_config_source": {
+                          "path": "resources/sds/xds-validation-context.json"
+                        }
                       }
                     }
                   }
