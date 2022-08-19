1abe6630772a144d48a9a89e370f8e6326b38c65
Tero Saarni <tero.saarni@est.tech>
2022-08-19 19:54:36 +0300

    Correct spelling mistakes caught by newer version of codespell (#4682)
    * Correct spelling mistakes caught by newer version of codespell
    
    * Add few workarounds for false positives, such as unfortunate character
      combinations in base64 encoded test data.
    
    * use actions-codespell@v1.0
    
    Signed-off-by: Tero Saarni <tero.saarni@est.tech>

--- internal/dag/policy.go
+++ internal/dag/policy.go
@@ -58,13 +58,13 @@ const (
 
 // retryOn transforms a slice of retry on values to a comma-separated string.
 // CRD validation ensures that all retry on values are valid.
-func retryOn(ro []contour_api_v1.RetryOn) string {
-	if len(ro) == 0 {
+func retryOn(ron []contour_api_v1.RetryOn) string {
+	if len(ron) == 0 {
 		return "5xx"
 	}
 
-	ss := make([]string, len(ro))
-	for i, value := range ro {
+	ss := make([]string, len(ron))
+	for i, value := range ron {
 		ss[i] = string(value)
 	}
 	return strings.Join(ss, ",")
