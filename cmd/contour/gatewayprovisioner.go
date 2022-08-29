6a6a685c1617d01d5f5451b684a1d3ed2aaafb0b
Steve Kriss <krisss@vmware.com>
2022-08-29 09:54:10 -0600

    update Envoy to v1.23.1 (#4691)
    Closes #4685.
    
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- cmd/contour/gatewayprovisioner.go
+++ cmd/contour/gatewayprovisioner.go
@@ -33,7 +33,7 @@ func registerGatewayProvisioner(app *kingpin.Application) (*kingpin.CmdClause, *
 
 	config := &gatewayProvisionerConfig{
 		contourImage:          "ghcr.io/projectcontour/contour:v1.22.0",
-		envoyImage:            "docker.io/envoyproxy/envoy:v1.23.0",
+		envoyImage:            "docker.io/envoyproxy/envoy:v1.23.1",
 		metricsBindAddress:    ":8080",
 		leaderElection:        false,
 		leaderElectionID:      "0d879e31.projectcontour.io",
