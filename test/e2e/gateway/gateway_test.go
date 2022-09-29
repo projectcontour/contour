90a56cbe6fd66d97adb6bb8493c8541051b805a3
Sunjay Bhatia <sunjayb@vmware.com>
2022-09-29 22:40:39 +0000

    Invalid backend ref test removed
    Covered by:
    -
    https://github.com/kubernetes-sigs/gateway-api/blob/main/conformance/tests/httproute-invalid-backendref-nonexistent.go
    -
    https://github.com/kubernetes-sigs/gateway-api/blob/main/conformance/tests/httproute-invalid-backendref-unknown-kind.go
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>

--- test/e2e/gateway/gateway_test.go
+++ test/e2e/gateway/gateway_test.go
@@ -202,8 +202,6 @@ func runGatewayTests() {
 
 		f.NamespacedTest("gateway-path-condition-match", testWithHTTPGateway(testGatewayPathConditionMatch))
 
-		f.NamespacedTest("gateway-invalid-forward-to", testWithHTTPGateway(testInvalidBackendRef))
-
 		f.NamespacedTest("gateway-request-header-modifier-forward-to", testWithHTTPGateway(testRequestHeaderModifierForwardTo))
 
 		f.NamespacedTest("gateway-request-header-modifier-rule", testWithHTTPGateway(testRequestHeaderModifierRule))
