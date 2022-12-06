d7d401280417e9d0721fd927bc1e364abbe63b67
Sunjay Bhatia <sunjayb@vmware.com>
2022-12-06 18:37:45 +0000

    Update Contour Docker image to v1.23.1.
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>

--- cmd/contour/gatewayprovisioner.go
+++ cmd/contour/gatewayprovisioner.go
@@ -33,7 +33,7 @@ func registerGatewayProvisioner(app *kingpin.Application) (*kingpin.CmdClause, *
 	cmd := app.Command("gateway-provisioner", "Run contour gateway provisioner.")
 
 	provisionerConfig := &gatewayProvisionerConfig{
-		contourImage:          "ghcr.io/projectcontour/contour:v1.23.0",
+		contourImage:          "ghcr.io/projectcontour/contour:v1.23.1",
 		envoyImage:            "docker.io/envoyproxy/envoy:v1.24.1",
 		metricsBindAddress:    ":8080",
 		leaderElection:        false,
