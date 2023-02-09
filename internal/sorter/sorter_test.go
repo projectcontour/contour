b1ff7be83dc3c4ab107a23fd21c5f2e972b320e7
Sunjay Bhatia <5337253+sunjayBhatia@users.noreply.github.com>
2023-02-09 17:52:38 -0500

    internal/sorter: Fix sorting for Query parameter matches (#5076)
    Also ensures query param match conditions on routes in sorted order
    
    Fixes a small issue in the header condition sorter as well.
    
    Sorting tests shuffle and sort data multiple times to help find
    order/stability issues and increase coverage.
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>

--- internal/sorter/sorter_test.go
+++ internal/sorter/sorter_test.go
@@ -30,15 +30,12 @@ import (
 	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
-func shuffleRoutes(routes []*dag.Route) []*dag.Route {
-	shuffled := make([]*dag.Route, len(routes))
-
-	copy(shuffled, routes)
-
-	rand.Shuffle(len(shuffled), func(i, j int) {
+func shuffleSlice[T any](original []T) []T {
+	shuffled := make([]T, len(original))
+	copy(shuffled, original)
+	rand.Shuffle(len(original), func(i, j int) {
 		shuffled[i], shuffled[j] = shuffled[j], shuffled[i]
 	})
-
 	return shuffled
 }
 
@@ -150,6 +147,58 @@ func presentHeader(name string) dag.HeaderMatchCondition {
 	}
 }
 
+func ignoreCaseQueryParam(h dag.QueryParamMatchCondition) dag.QueryParamMatchCondition {
+	h.IgnoreCase = true
+	return h
+}
+
+func exactQueryParam(name string, value string) dag.QueryParamMatchCondition {
+	return dag.QueryParamMatchCondition{
+		Name:      name,
+		MatchType: dag.QueryParamMatchTypeExact,
+		Value:     value,
+	}
+}
+
+func prefixQueryParam(name string, value string) dag.QueryParamMatchCondition {
+	return dag.QueryParamMatchCondition{
+		Name:      name,
+		MatchType: dag.QueryParamMatchTypePrefix,
+		Value:     value,
+	}
+}
+
+func suffixQueryParam(name string, value string) dag.QueryParamMatchCondition {
+	return dag.QueryParamMatchCondition{
+		Name:      name,
+		MatchType: dag.QueryParamMatchTypeSuffix,
+		Value:     value,
+	}
+}
+
+func regexQueryParam(name string, value string) dag.QueryParamMatchCondition {
+	return dag.QueryParamMatchCondition{
+		Name:      name,
+		MatchType: dag.QueryParamMatchTypeRegex,
+		Value:     value,
+	}
+}
+
+func containsQueryParam(name string, value string) dag.QueryParamMatchCondition {
+	return dag.QueryParamMatchCondition{
+		Name:      name,
+		MatchType: dag.QueryParamMatchTypeContains,
+		Value:     value,
+	}
+}
+
+func presentQueryParam(name string) dag.QueryParamMatchCondition {
+	return dag.QueryParamMatchCondition{
+		Name:      name,
+		MatchType: dag.QueryParamMatchTypePresent,
+	}
+}
+
 // Routes that have a higher priority should be sorted first when compared to
 // others that have identical path matches, number of header matches, and
 // number of query matches.
@@ -189,7 +238,7 @@ func TestSortRoutesPriority(t *testing.T) {
 			Priority:           2,
 			PathMatchCondition: matchPrefixSegment("/"),
 			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
-				{Name: "z-query-param", Value: "query-value"},
+				containsQueryParam("z-query-param", "query-value"),
 			},
 		},
 		{
@@ -198,14 +247,14 @@ func TestSortRoutesPriority(t *testing.T) {
 			Priority:           3,
 			PathMatchCondition: matchPrefixSegment("/"),
 			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
-				{Name: "a-query-param", Value: "query-value"},
+				exactQueryParam("a-query-param", "query-value"),
 			},
 		},
 		{
 			Priority:           3,
 			PathMatchCondition: matchPrefixSegment("/"),
 			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
-				{Name: "b-query-param", Value: "query-value"},
+				exactQueryParam("b-query-param", "query-value"),
 			},
 		},
 		{
@@ -213,11 +262,7 @@ func TestSortRoutesPriority(t *testing.T) {
 			PathMatchCondition: matchPrefixSegment("/"),
 		},
 	}
-
-	have := shuffleRoutes(want)
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortRoutesPathMatch(t *testing.T) {
@@ -265,15 +310,29 @@ func TestSortRoutesPathMatch(t *testing.T) {
 			PathMatchCondition: matchPrefixSegment("/path/p"),
 		},
 	}
-
-	have := shuffleRoutes(want)
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortRoutesLongestHeaders(t *testing.T) {
 	want := []*dag.Route{
+		{
+			// More conditions sort higher.
+			PathMatchCondition: matchExact("/pathexact"),
+			HeaderMatchConditions: []dag.HeaderMatchCondition{
+				exactHeader("header-name", "header-value"),
+				exactHeader("header-name-2", "header-value"),
+				exactHeader("header-name-3", "header-value"),
+			},
+		},
+		{
+			// Per-element name comparison means this sorts later.
+			PathMatchCondition: matchExact("/pathexact"),
+			HeaderMatchConditions: []dag.HeaderMatchCondition{
+				exactHeader("header-name", "header-value"),
+				exactHeader("header-name-2", "header-value"),
+				exactHeader("header-name-999", "header-value"),
+			},
+		},
 		{
 			// Although the header names are the same, this value
 			// should sort before the next one because it is
@@ -347,65 +406,101 @@ func TestSortRoutesLongestHeaders(t *testing.T) {
 			PathMatchCondition: matchPrefixString("/path"),
 		},
 	}
-
-	have := shuffleRoutes(want)
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortRoutesQueryParams(t *testing.T) {
 	want := []*dag.Route{
 		{
-
 			PathMatchCondition: matchExact("/"),
+			// More match conditions sort higher.
 			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
-				{Name: "query-param-1", Value: "query-value-1"},
-				{Name: "query-param-2", Value: "query-value-2"},
-				{Name: "query-param-3", Value: "query-value-3"},
+				containsQueryParam("query-param-1", "query-value-1"),
+				containsQueryParam("query-param-2", "query-value-2"),
+				containsQueryParam("query-param-3", "query-value-3"),
 			},
 		},
 		{
-
 			PathMatchCondition: matchExact("/"),
 			// If same number of matches, sort on element-by-element
-			// comparison of each query param name provided.
+			// comparison of query param name.
 			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
-				{Name: "aaa-query-param-1", Value: "query-value-1"},
-				{Name: "query-param-2", Value: "query-value-2"},
+				exactQueryParam("aaa-param", "val"),
+				exactQueryParam("query-param-1", "query-value-1"),
 			},
 		},
 		{
-
 			PathMatchCondition: matchExact("/"),
-			// If same number of matches, sort on element-by-element
-			// comparison of each query param name provided.
 			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
-				{Name: "query-param-1", Value: "query-value-1"},
-				{Name: "bbb-query-param-2", Value: "query-value-2"},
+				exactQueryParam("bbb-param", "val"),
+				exactQueryParam("query-param-1", "query-value-1"),
 			},
 		},
 		{
-
 			PathMatchCondition: matchExact("/"),
+			// Exact is most specific.
 			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
-				{Name: "query-param-1", Value: "query-value-1"},
-				{Name: "query-param-2", Value: "query-value-2"},
+				exactQueryParam("query-param-1", "query-value-1"),
 			},
 		},
 		{
-
+			PathMatchCondition: matchExact("/"),
+			// If matching type, sort on value.
+			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
+				exactQueryParam("query-param-1", "query-value-1-other"),
+			},
+		},
+		{
+			PathMatchCondition: matchExact("/"),
+			// If matching type, value, sort on case sensitivity.
+			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
+				ignoreCaseQueryParam(exactQueryParam("query-param-1", "query-value-1-other")),
+			},
+		},
+		{
 			PathMatchCondition: matchExact("/"),
 			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
-				{Name: "query-param-1", Value: "query-value-1"},
+				regexQueryParam("query-param-1", "query-value-1"),
+			},
+		},
+		{
+			PathMatchCondition: matchExact("/"),
+			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
+				prefixQueryParam("query-param-1", "query-value-1"),
+			},
+		},
+		{
+			PathMatchCondition: matchExact("/"),
+			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
+				suffixQueryParam("query-param-1", "query-value-1"),
+			},
+		},
+		{
+			PathMatchCondition: matchExact("/"),
+			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
+				containsQueryParam("query-param-1", "query-value-1"),
+			},
+		},
+		{
+			PathMatchCondition: matchExact("/"),
+			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
+				presentQueryParam("query-param-1"),
+			},
+		},
+		{
+			PathMatchCondition: matchExact("/"),
+			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
+				prefixQueryParam("query-param-2", "query-value-2"),
+			},
+		},
+		{
+			PathMatchCondition: matchExact("/"),
+			QueryParamMatchConditions: []dag.QueryParamMatchCondition{
+				prefixQueryParam("query-param-2", "query-value-2-after"),
 			},
 		},
 	}
-
-	have := shuffleRoutes(want)
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortSecrets(t *testing.T) {
@@ -413,14 +508,7 @@ func TestSortSecrets(t *testing.T) {
 		{Name: "first"},
 		{Name: "second"},
 	}
-
-	have := []*envoy_tls_v3.Secret{
-		want[1],
-		want[0],
-	}
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortHeaderMatchConditions(t *testing.T) {
@@ -435,18 +523,7 @@ func TestSortHeaderMatchConditions(t *testing.T) {
 		presentHeader("header-name"),
 		exactHeader("long-header-name", "long-header-value"),
 	}
-
-	have := []dag.HeaderMatchCondition{
-		want[5],
-		want[4],
-		want[3],
-		want[0],
-		want[2],
-		want[1],
-	}
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortHeaderMatchConditionsInverted(t *testing.T) {
@@ -459,18 +536,7 @@ func TestSortHeaderMatchConditionsInverted(t *testing.T) {
 		presentHeader("header-name"),
 		invertHeaderMatch(presentHeader("header-name")),
 	}
-
-	have := []dag.HeaderMatchCondition{
-		want[5],
-		want[4],
-		want[3],
-		want[2],
-		want[1],
-		want[0],
-	}
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortHeaderMatchConditionsValue(t *testing.T) {
@@ -485,20 +551,31 @@ func TestSortHeaderMatchConditionsValue(t *testing.T) {
 		containsHeader("header-name", "b"),
 		containsHeader("header-name", "c"),
 	}
+	shuffleAndCheckSort(t, want)
+}
 
-	have := []dag.HeaderMatchCondition{
-		want[6],
-		want[5],
-		want[4],
-		want[7],
-		want[2],
-		want[1],
-		want[0],
-		want[3],
+func TestSortQueryParamMatchConditionsValue(t *testing.T) {
+	want := []dag.QueryParamMatchCondition{
+		exactQueryParam("query-name", "a"),
+		ignoreCaseQueryParam(exactQueryParam("query-name", "a")),
+		exactQueryParam("query-name", "b"),
+		exactQueryParam("query-name", "c"),
+		regexQueryParam("query-name", "a"),
+		ignoreCaseQueryParam(regexQueryParam("query-name", "a")),
+		regexQueryParam("query-name", "b"),
+		prefixQueryParam("query-name", "a"),
+		ignoreCaseQueryParam(prefixQueryParam("query-name", "a")),
+		prefixQueryParam("query-name", "b"),
+		suffixQueryParam("query-name", "a"),
+		ignoreCaseQueryParam(suffixQueryParam("query-name", "a")),
+		suffixQueryParam("query-name", "b"),
+		containsQueryParam("query-name", "a"),
+		ignoreCaseQueryParam(containsQueryParam("query-name", "a")),
+		containsQueryParam("query-name", "b"),
+		containsQueryParam("query-name", "c"),
+		presentQueryParam("query-name"),
 	}
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortClusters(t *testing.T) {
@@ -506,14 +583,7 @@ func TestSortClusters(t *testing.T) {
 		{Name: "first"},
 		{Name: "second"},
 	}
-
-	have := []*envoy_cluster_v3.Cluster{
-		want[1],
-		want[0],
-	}
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortClusterLoadAssignments(t *testing.T) {
@@ -521,14 +591,7 @@ func TestSortClusterLoadAssignments(t *testing.T) {
 		{ClusterName: "first"},
 		{ClusterName: "second"},
 	}
-
-	have := []*envoy_endpoint_v3.ClusterLoadAssignment{
-		want[1],
-		want[0],
-	}
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortHTTPWeightedClusters(t *testing.T) {
@@ -546,15 +609,7 @@ func TestSortHTTPWeightedClusters(t *testing.T) {
 			Weight: wrapperspb.UInt32(20),
 		},
 	}
-
-	have := []*envoy_route_v3.WeightedCluster_ClusterWeight{
-		want[2],
-		want[1],
-		want[0],
-	}
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortTCPWeightedClusters(t *testing.T) {
@@ -572,15 +627,7 @@ func TestSortTCPWeightedClusters(t *testing.T) {
 			Weight: 20,
 		},
 	}
-
-	have := []*tcp.TcpProxy_WeightedCluster_ClusterWeight{
-		want[2],
-		want[1],
-		want[0],
-	}
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortListeners(t *testing.T) {
@@ -588,14 +635,7 @@ func TestSortListeners(t *testing.T) {
 		{Name: "first"},
 		{Name: "second"},
 	}
-
-	have := []*envoy_listener_v3.Listener{
-		want[1],
-		want[0],
-	}
-
-	sort.Stable(For(have))
-	assert.Equal(t, want, have)
+	shuffleAndCheckSort(t, want)
 }
 
 func TestSortFilterChains(t *testing.T) {
@@ -634,4 +674,16 @@ func TestSortFilterChains(t *testing.T) {
 
 	sort.Stable(For(have))
 	assert.Equal(t, want, have)
+}
+
+func shuffleAndCheckSort[T any](t *testing.T, want []T) {
+	t.Helper()
+
+	// Run multiple trials so we catch any ordering/stability errors.
+	for i := 0; i < 10; i++ {
+		have := shuffleSlice(want)
+
+		sort.Stable(For(have))
+		assert.Equal(t, want, have)
+	}
 }
