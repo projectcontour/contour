7606bd255c0d5bd93cc859a98c9a08a4996694d3
Nikhil Barik <36399086+nerdynikhil@users.noreply.github.com>
2023-03-08 10:55:38 +0530

    fix
    Signed-off-by: Nikhil Barik <36399086+nerdynikhil@users.noreply.github.com>

--- test/scripts/install-provisioner-working.sh
+++ test/scripts/install-provisioner-working.sh
@@ -35,11 +35,29 @@ VERSION="v$$"
 make -C ${REPO} container IMAGE=ghcr.io/projectcontour/contour VERSION=${VERSION}
 
 # Push the Contour Provisioner image into the cluster.
-kind::cluster::load::docker ghcr.io/projectcontour/contour-provisioner:${VERSION}
+kind::cluster::load::docker ghcr.io/projectcontour/contour:${VERSION}
 
-# Install the Contour Provisioner.
-${KUBECTL} apply -f ${REPO}/examples/gateway-provisioner/deploy.yaml
-${KUBECTL} apply -f ${REPO}/examples/gateway-provisioner/service.yaml
+for file in ${REPO}/examples/gateway-provisioner/00-common.yaml ${REPO}/examples/gateway-provisioner/01-roles.yaml ${REPO}/examples/gateway-provisioner/02-rolebindings.yaml ${REPO}/examples/gateway-provisioner/03-gateway-provisioner.yaml ; do
+  # Set image pull policy to IfNotPresent so kubelet will use the
+  # images that we loaded onto the node, rather than trying to pull
+  # them from the registry.
+  run::sed \
+    "-es|imagePullPolicy: Always|imagePullPolicy: IfNotPresent|" \
+    "$file"
+
+  # Set the image tag to $VERSION to unambiguously use the image
+  # we built above.
+  run::sed \
+    "-es|image: ghcr.io/projectcontour/contour:.*$|image: ghcr.io/projectcontour/contour:${VERSION}|" \
+    "$file"
+
+  # Add an item to the args field with the value --contour-image=ghcr.io/projectcontour/contour:${VERSION}
+  run::sed \
+    "-e 's/- --ingress-class=contour/- --ingress-class=contour\n        - --contour-image=ghcr.io\/projectcontour\/contour:${VERSION}/' \
+    "$file"
+
+  ${KUBECTL} apply -f "$file"
+done
 
 # Wait for the provisioner to report "Ready" status.
 ${KUBECTL} wait --timeout="${WAITTIME}" -n projectcontour -l control-plane=contour-gateway-provisioner deployments --for=condition=Available
