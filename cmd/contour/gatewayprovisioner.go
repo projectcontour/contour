437db66f257db70561d2cc3dd463b2f892604239
Steve Kriss <krisss@vmware.com>
2023-03-14 10:37:14 -0600

    bump Envoy to v1.23.5
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- cmd/contour/gatewayprovisioner.go
+++ cmd/contour/gatewayprovisioner.go
@@ -33,7 +33,7 @@ func registerGatewayProvisioner(app *kingpin.Application) (*kingpin.CmdClause, *
 
 	config := &gatewayProvisionerConfig{
 		contourImage:          "ghcr.io/projectcontour/contour:v1.22.4",
-		envoyImage:            "docker.io/envoyproxy/envoy:v1.23.4",
+		envoyImage:            "docker.io/envoyproxy/envoy:v1.23.5",
 		metricsBindAddress:    ":8080",
 		leaderElection:        false,
 		leaderElectionID:      "0d879e31.projectcontour.io",
