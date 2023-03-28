b0925f0abdf8dec43c7e435bcefbe5df7fc2bc59
Steve Kriss <krisss@vmware.com>
2023-03-28 11:59:00 -0600

    test/scripts: fix upgrade from version
    Updates #5184.
    
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- test/scripts/get-contour-upgrade-from-version.sh
+++ test/scripts/get-contour-upgrade-from-version.sh
@@ -7,11 +7,17 @@ set -o pipefail
 if CURRENT_TAG=$(git describe --tags --exact-match 2>/dev/null); then
   # We are on a tag, so find previous tag to this one.
   git tag -l --sort=-v:refname | grep -v 'alpha\|beta\|rc' | grep -A1 -x $CURRENT_TAG | tail -1
-elif git branch --show-current | grep -q release; then
-  # We are on a release branch, so find tag.
+elif git describe --tags --abbrev=0 | grep -q -v v1.2.0; then
+  # Note: Contour v1.2.0 was improperly tagged on main so we
+  # ignore it to ensure we dont hit that case here.
+
+  # We have a tag in our commit history, so we should
+  # be on a release branch or a feature branch from a
+  # release branch, with an existing tag.
   git describe --tags --abbrev=0
 else
-  # We are likely on main or some other checkout, just use latest tag.
+  # We are on a release branch with no tag created yet, main, or some
+  # other checkout, so just use the latest tag.
   # If needed, user can override this version with environment variables.
   git tag -l --sort=-v:refname | grep -v 'alpha\|beta\|rc' | head -1
 fi
