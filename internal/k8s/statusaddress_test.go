a61f48377a7cded8210acce2212e13f69c627e80
Sunjay Bhatia <sunjayb@vmware.com>
2023-06-16 16:29:08 +0000

    More test logger fixes
    Move to using fixture.NewTestlogger|NewDiscardLogger
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>

--- internal/k8s/statusaddress_test.go
+++ internal/k8s/statusaddress_test.go
@@ -351,7 +351,7 @@ func TestStatusAddressUpdater(t *testing.T) {
 
 //go:generate go run github.com/vektra/mockery/v2 --case=snake --name=Cache --srcpkg=sigs.k8s.io/controller-runtime/pkg/cache  --disable-version-string
 func TestStatusAddressUpdater_Gateway(t *testing.T) {
-	log := logrus.New()
+	log := fixture.NewTestLogger(t)
 	log.SetLevel(logrus.DebugLevel)
 
 	ipLBStatus := v1.LoadBalancerStatus{
