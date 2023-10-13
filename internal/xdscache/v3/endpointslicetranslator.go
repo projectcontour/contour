362a5ca772477bdad7a68ef1aeba24266a40a3f0
Clayton Gonsalves <clayton.gonsalves@reddit.com>
2023-10-13 17:44:11 +0200

    update endpoint.address description
    Signed-off-by: Clayton Gonsalves <clayton.gonsalves@reddit.com>

--- internal/xdscache/v3/endpointslicetranslator.go
+++ internal/xdscache/v3/endpointslicetranslator.go
@@ -76,6 +76,13 @@ func (c *EndpointSliceCache) RecalculateEndpoints(port, healthPort v1.ServicePor
 				}
 
 				// we can safely take the first element here.
+				// Refer k8s API description:
+				// The contents of this field are interpreted according to
+				// the corresponding EndpointSlice addressType field.
+				// Consumers must handle different types of addresses in the context
+				// of their own capabilities. This must contain at least one
+				// address but no more than 100. These are all assumed to be fungible
+				// and clients may choose to only use the first element.
 				// Refer to: https://issue.k8s.io/106267
 				addr := envoy_v3.SocketAddress(endpoint.Addresses[0], int(*p.Port))
 
