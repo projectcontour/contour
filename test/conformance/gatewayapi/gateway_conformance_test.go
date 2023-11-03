53ed202af74c69571a962fd63c4d2fba6c819706
gang.liu <gang.liu@daocloud.io>
2023-11-03 17:31:57 +0800

    skip HTTPRoute ws check
    Signed-off-by: gang.liu <gang.liu@daocloud.io>

--- test/conformance/gatewayapi/gateway_conformance_test.go
+++ test/conformance/gatewayapi/gateway_conformance_test.go
@@ -75,7 +75,7 @@ func TestGatewayConformance(t *testing.T) {
 			tests.HTTPRouteRedirectPortAndScheme.ShortName,
 
 			// Tests newly failing with Gateway API 1.0, to be addressed.
-			tests.HTTPRouteBackendProtocolH2C.ShortName,
+			tests.HTTPRouteBackendProtocolWebSocket.ShortName,
 			tests.HTTPRouteTimeoutBackendRequest.ShortName,
 			tests.HTTPRouteTimeoutRequest.ShortName,
 			tests.GatewayWithAttachedRoutes.ShortName,
