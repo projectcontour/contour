8e40b37d95a99e6f3ec6d780c0b9ff9f835432af
Steve Kriss <krisss@vmware.com>
2023-11-07 11:49:23 -0700

    bump golangci-lint to v1.55.2 and fix new lint issues (#5941)
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- internal/dag/policy_test.go
+++ internal/dag/policy_test.go
@@ -644,6 +644,7 @@ func TestHeadersPolicy(t *testing.T) {
 	}
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
+			tc := tc
 			got, gotErr := headersPolicyService(&tc.dhp, tc.hp, true, dynamicHeaders)
 			if tc.wantErr {
 				assert.Error(t, gotErr)
