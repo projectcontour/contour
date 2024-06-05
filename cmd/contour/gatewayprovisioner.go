f91a3287ca8681ace6ef75f3e1296299301481f9
Sunjay Bhatia <5337253+sunjayBhatia@users.noreply.github.com>
2024-06-05 14:41:40 -0400

    Bump Envoy to 1.30.2 (#6484)
    For CVE fixes
    
    See release notes: https://www.envoyproxy.io/docs/envoy/v1.30.2/version_history/v1.30/v1.30.2
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>

--- cmd/contour/gatewayprovisioner.go
+++ cmd/contour/gatewayprovisioner.go
@@ -36,7 +36,7 @@ func registerGatewayProvisioner(app *kingpin.Application) (*kingpin.CmdClause, *
 
 	provisionerConfig := &gatewayProvisionerConfig{
 		contourImage:          "ghcr.io/projectcontour/contour:v1.29.0",
-		envoyImage:            "docker.io/envoyproxy/envoy:v1.30.1",
+		envoyImage:            "docker.io/envoyproxy/envoy:v1.30.2",
 		metricsBindAddress:    ":8080",
 		leaderElection:        false,
 		leaderElectionID:      "0d879e31.projectcontour.io",
