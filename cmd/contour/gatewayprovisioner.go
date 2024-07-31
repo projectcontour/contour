9773f70b86e59b615b078d2cad9e049a571d3852
Steve Kriss <stephen.kriss@gmail.com>
2024-07-31 12:30:10 -0600

    Update Contour Docker image to v1.28.6.
    Signed-off-by: Steve Kriss <stephen.kriss@gmail.com>

--- cmd/contour/gatewayprovisioner.go
+++ cmd/contour/gatewayprovisioner.go
@@ -35,7 +35,7 @@ func registerGatewayProvisioner(app *kingpin.Application) (*kingpin.CmdClause, *
 	cmd := app.Command("gateway-provisioner", "Run contour gateway provisioner.")
 
 	provisionerConfig := &gatewayProvisionerConfig{
-		contourImage:          "ghcr.io/projectcontour/contour:v1.28.5",
+		contourImage:          "ghcr.io/projectcontour/contour:v1.28.6",
 		envoyImage:            "docker.io/envoyproxy/envoy:v1.29.7",
 		metricsBindAddress:    ":8080",
 		leaderElection:        false,
