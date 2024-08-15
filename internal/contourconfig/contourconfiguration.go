ssttehrani/contour-spcld

set stat_prefix if enableStatPrefix is true
Signed-off-by: Ahmad Karimi <ak12hastam@gmail.com>

internal/contourconfig/contourconfiguration.go
@@ -18,6 +18,7 @@ import (
 	"time"
 
 	"dario.cat/mergo"
+
 	contour_api_v1alpha1 "github.com/projectcontour/contour/apis/projectcontour/v1alpha1"
 	"github.com/projectcontour/contour/internal/ref"
 	"github.com/projectcontour/contour/internal/timeout"
@@ -125,6 +126,7 @@ func Defaults() contour_api_v1alpha1.ContourConfigurationSpec {
 				XffNumTrustedHops: ref.To(uint32(0)),
 				EnvoyAdminPort:    ref.To(9001),
 			},
+			EnableStatPrefix: ref.To(false),
 		},
 		Gateway: nil,
 		HTTPProxy: &contour_api_v1alpha1.HTTPProxyConfig{

internal/dag/gatewayapi_processor.go
@@ -70,6 +70,9 @@ type GatewayAPIProcessor struct {
 	// configurable and off by default in order to support the feature
 	// without requiring all existing test cases to change.
 	SetSourceMetadataOnRoutes bool
+
+	// Whether to set StatPrefix on envoy routes or not
+	EnableStatPrefix bool
 }
 
 // matchConditions holds match rules.
@@ -2086,6 +2089,10 @@ func (p *GatewayAPIProcessor) clusterRoutes(
 			route.Name = name
 		}
 
+		if p.EnableStatPrefix {
+			route.StatPrefix = ref.To(fmt.Sprintf("%s_%s", namespace, name))
+		}
+
 		routes = append(routes, route)
 	}
 
@@ -2152,6 +2159,10 @@ func (p *GatewayAPIProcessor) redirectRoutes(
 			route.Kind = kind
 			route.Namespace = namespace
 			route.Name = name
+		}
+
+		if p.EnableStatPrefix {
+			route.StatPrefix = ref.To(fmt.Sprintf("%s_%s", namespace, name))
 		}
 
 		routes = append(routes, route)

projectcontour/contour

Signed-off-by: René Treffer <rene.treffer@reddit.com>

internal/contourconfig/contourconfiguration.go
@@ -131,6 +131,7 @@ func Defaults() contour_v1alpha1.ContourConfigurationSpec {
 				XffNumTrustedHops: ptr.To(uint32(0)),
 				EnvoyAdminPort:    ptr.To(9001),
 			},
+			EnableStatPrefix: ptr.To(false),
 		},
 		Gateway: nil,
 		HTTPProxy: &contour_v1alpha1.HTTPProxyConfig{

internal/dag/gatewayapi_processor.go
@@ -81,6 +81,9 @@ type GatewayAPIProcessor struct {
 	// UpstreamTLS defines the TLS settings like min/max version
 	// and cipher suites for upstream connections.
 	UpstreamTLS *UpstreamTLS
+
+	// Whether to set StatPrefix on envoy routes or not
+	EnableStatPrefix bool
 }
 
 // matchConditions holds match rules.
@@ -2405,6 +2408,10 @@ func (p *GatewayAPIProcessor) clusterRoutes(
 			route.Name = name
 		}
 
+		if p.EnableStatPrefix {
+			route.StatPrefix = ptr.To(fmt.Sprintf("%s_%s", namespace, name))
+		}
+
 		routes = append(routes, route)
 	}
 
@@ -2471,6 +2478,10 @@ func (p *GatewayAPIProcessor) redirectRoutes(
 			route.Kind = kind
 			route.Namespace = namespace
 			route.Name = name
+		}
+
+		if p.EnableStatPrefix {
+			route.StatPrefix = ptr.To(fmt.Sprintf("%s_%s", namespace, name))
 		}
 
 		routes = append(routes, route)