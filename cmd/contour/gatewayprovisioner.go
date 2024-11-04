a1a414282974f1061e2cb8d371bbab1218bbfa61
Tero Saarni <tero.saarni@est.tech>
2024-11-04 13:58:22 +0200

    Update Contour Docker image to v1.30.1.
    Signed-off-by: Tero Saarni <tero.saarni@est.tech>

--- cmd/contour/gatewayprovisioner.go
+++ cmd/contour/gatewayprovisioner.go
@@ -35,7 +35,7 @@ func registerGatewayProvisioner(app *kingpin.Application) (*kingpin.CmdClause, *
 	cmd := app.Command("gateway-provisioner", "Run contour gateway provisioner.")
 
 	provisionerConfig := &gatewayProvisionerConfig{
-		contourImage:          "ghcr.io/projectcontour/contour:v1.30.0",
+		contourImage:          "ghcr.io/projectcontour/contour:v1.30.1",
 		envoyImage:            "docker.io/envoyproxy/envoy:v1.31.3",
 		metricsBindAddress:    ":8080",
 		leaderElection:        false,
