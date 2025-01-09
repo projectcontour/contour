138ecd6d28598ec3af427eb30ff999cb1b2d5d1a
Sunjay Bhatia <sunjay.bhatia@broadcom.com>
2025-01-09 16:12:37 -0500

    Bump Envoy to 1.30.9
    See release notes: https://www.envoyproxy.io/docs/envoy/v1.30.9/version_history/v1.30/v1.30.9
    
    Signed-off-by: Sunjay Bhatia <sunjay.bhatia@broadcom.com>

--- cmd/contour/gatewayprovisioner.go
+++ cmd/contour/gatewayprovisioner.go
@@ -36,7 +36,7 @@ func registerGatewayProvisioner(app *kingpin.Application) (*kingpin.CmdClause, *
 
 	provisionerConfig := &gatewayProvisionerConfig{
 		contourImage:          "ghcr.io/projectcontour/contour:v1.29.3",
-		envoyImage:            "docker.io/envoyproxy/envoy:v1.30.7",
+		envoyImage:            "docker.io/envoyproxy/envoy:v1.30.9",
 		metricsBindAddress:    ":8080",
 		leaderElection:        false,
 		leaderElectionID:      "0d879e31.projectcontour.io",
