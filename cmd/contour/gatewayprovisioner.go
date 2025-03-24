5bfa56f3412fab4083f9e89bebc3bba0f63e31f0
Tero Saarni <tero.saarni@est.tech>
2025-03-24 10:58:43 +0200

    Bumped Envoy to 1.30.10
    Signed-off-by: Tero Saarni <tero.saarni@est.tech>

--- cmd/contour/gatewayprovisioner.go
+++ cmd/contour/gatewayprovisioner.go
@@ -36,7 +36,7 @@ func registerGatewayProvisioner(app *kingpin.Application) (*kingpin.CmdClause, *
 
 	provisionerConfig := &gatewayProvisionerConfig{
 		contourImage:          "ghcr.io/projectcontour/contour:v1.29.4",
-		envoyImage:            "docker.io/envoyproxy/envoy:v1.30.9",
+		envoyImage:            "docker.io/envoyproxy/envoy:v1.30.10",
 		metricsBindAddress:    ":8080",
 		leaderElection:        false,
 		leaderElectionID:      "0d879e31.projectcontour.io",
