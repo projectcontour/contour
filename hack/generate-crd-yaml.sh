b7cb11f20259687a8c1f3e65fc8d0c8e4bb4dd9b
Tero Saarni <tero.saarni@est.tech>
2026-02-13 11:29:00 +0200

    Post-process HTTPProxy CRD schema to remove error as a required field (#7408) (#7410)
    Signed-off-by: Tero Saarni <tero.saarni@est.tech>

--- hack/generate-crd-yaml.sh
+++ hack/generate-crd-yaml.sh
@@ -24,6 +24,25 @@ go run sigs.k8s.io/controller-tools/cmd/controller-gen --version
 go run sigs.k8s.io/controller-tools/cmd/controller-gen \
   crd:crdVersions=v1 "paths=${PATHS}" "output:dir=${TEMPDIR}"
 
+# Remove "error" from required fields in load balancer status.
+# For details, see:
+# - https://github.com/projectcontour/contour/issues/7391
+# - https://github.com/kubernetes-sigs/controller-tools/pull/944#issuecomment-3314629362
+# This workaround can be removed if the upstream Kubernetes type resolves the conflicting markers.
+readonly HTTPPROXY_CRD="${TEMPDIR}/projectcontour.io_httpproxies.yaml"
+readonly PATCH_PATH="/spec/versions/0/schema/openAPIV3Schema/properties/status/properties/loadBalancer/properties/ingress/items/properties/ports/items/required/0"
+
+kubectl::patch() {
+  kubectl patch -f "$HTTPPROXY_CRD" --local --type=json -p "$1" "${@:2}"
+}
+
+kubectl::patch "[{\"op\": \"test\", \"path\": \"$PATCH_PATH\", \"value\": \"error\"}]" --dry-run=client > /dev/null || {
+  echo "Error: CRD structure has changed. The workaround for issue #7391 may no longer be needed or needs updating."
+  exit 1
+}
+
+kubectl::patch "[{\"op\": \"remove\", \"path\": \"$PATCH_PATH\"}]" -o yaml > "$HTTPPROXY_CRD.tmp" && { echo "---"; cat "$HTTPPROXY_CRD.tmp"; } > "$HTTPPROXY_CRD"
+
 # Explicitly add "preserveUnknownFields: false" to CRD specs since any CRDs created
 # as v1beta1 will have this field set to true, which we don't want going forward, and
 # it needs to be explicitly specified in order to be updated/removed. After enough time
